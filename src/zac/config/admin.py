ab26c8ef49678c2f4b608fba1ddb91003f5d81b1
annashamray <anna.shamray@maykinmedia.nl>
2019-09-27 15:45:35 +0200

    create RegieZaakConfiguratie model and register it in the admin

---
+++ src/zac/config/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from zgw_consumers.admin import ListZaaktypenMixin
+
+from .models import RegieZaakConfiguratie
+
+
+@admin.register(RegieZaakConfiguratie)
+class RegieZaakConfiguratieAdmin(ListZaaktypenMixin, admin.ModelAdmin):
+    list_display = ('name',)
+    zaaktype_fields = ('zaaktype_main',)
---
+++ src/zac/config/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django import forms
+from zgw_consumers.admin_fields import get_zaaktypen
+
+
+class ZaakTypeArrayField(ArrayField):
+    def formfield(self, **kwargs):
+        zaaktypen = get_zaaktypen()
+
+        choices = [
+            (
+                f"Service: {service.label}",
+                [
+                    (
+                        zaaktype["url"],
+                        f"{zaaktype['identificatie']} - {zaaktype['omschrijving']}",
+                    )
+                    for zaaktype in _zaaktypen
+                ],
+            )
+            for service, _zaaktypen in zaaktypen.items()
+        ]
+
+        defaults = {
+            'form_class': forms.MultipleChoiceField,
+            'choices': choices,
+            'widget': forms.CheckboxSelectMultiple,
+        }
+        defaults.update(kwargs)
+        return super(ArrayField, self).formfield(**defaults)
+
+
+class RegieZaakConfiguratie(models.Model):
+    name = models.CharField(max_length=100, help_text="The name of the theme/subject")
+    zaaktype_main = models.URLField()
+    related_zaaktypes = ZaakTypeArrayField(base_field=models.URLField(), default=list())
+
+    class Meta:
+        verbose_name = "Regie Zaak"
+
+
