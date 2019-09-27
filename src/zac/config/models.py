bd48942dc25b40a250691c02feb94676c1adab06
annashamray <anna.shamray@maykinmedia.nl>
2019-09-27 16:09:03 +0200

    add validation that main zaaktype is not dublicated in related zaaktypes

--- src/zac/config/models.py
+++ src/zac/config/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.postgres.fields import ArrayField
 from django import forms
 from zgw_consumers.admin_fields import get_zaaktypen
@@ -34,9 +35,15 @@ class ZaakTypeArrayField(ArrayField):
 class RegieZaakConfiguratie(models.Model):
     name = models.CharField(max_length=100, help_text="The name of the theme/subject")
     zaaktype_main = models.URLField()
-    related_zaaktypes = ZaakTypeArrayField(base_field=models.URLField(), default=list())
+    zaaktypes_related = ZaakTypeArrayField(base_field=models.URLField(), default=list())
 
     class Meta:
         verbose_name = "Regie Zaak"
 
+    def clean(self):
+        super().clean()
 
+        if self.zaaktype_main in self.zaaktypes_related:
+            raise ValidationError(
+                "'zaaktypes_related' can't include 'zaaktype_main'"
+            )
