aa56549f603e1951ae528ea43bfc17f4575b9a75
Sergei Maertens <sergei@maykinmedia.nl>
2019-10-21 11:29:27 +0200

    :sparkles: Enable django-import-export

---
+++ src/zac/utils/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+from django_camunda.admin import CamundaConfigAdmin
+from django_camunda.models import CamundaConfig
+from import_export.admin import ImportExportMixin
+from nlx_url_rewriter.admin import URLRewriteAdmin
+from nlx_url_rewriter.models import URLRewrite
+from zgw_consumers.admin import ServiceAdmin
+from zgw_consumers.models import Service
+
+from .resources import (
+    CamundaConfigResource, ServiceResource, URLRewriteResource
+)
+
+admin.site.unregister(Service)
+admin.site.unregister(URLRewrite)
+admin.site.unregister(CamundaConfig)
+
+
+class ImportExportSoloMixin(ImportExportMixin):
+    change_form_template = "utils/import_export_change_form.html"
+
+
+@admin.register(Service)
+class ServiceResourceAdmin(ImportExportMixin, ServiceAdmin):
+    resource_class = ServiceResource
+
+
+@admin.register(URLRewrite)
+class URLRewriteResourceAdmin(ImportExportMixin, URLRewriteAdmin):
+    resource_class = URLRewriteResource
+
+
+@admin.register(CamundaConfig)
+class CamundaConfigResourceAdmin(ImportExportSoloMixin, CamundaConfigAdmin):
+    resource_class = CamundaConfigResource
---
+++ src/zac/utils/resources.py
@@ -0,0 +1,51 @@
+import json
+
+from django_camunda.models import CamundaConfig
+from import_export import resources, widgets
+from nlx_url_rewriter.models import URLRewrite
+from zgw_consumers.models import Service
+
+
+class JSONWidget(widgets.Widget):
+    """ Convert data into JSON for serialization."""
+
+    def clean(self, value, row=None, *args, **kwargs):
+        return json.loads(value)
+
+    def render(self, value, obj=None):
+        if value is None:
+            return ""
+        return json.dumps(value)
+
+
+class JSONResourceMixin(object):
+    """ Override ModelResource to provide JSON field support."""
+
+    @classmethod
+    def widget_from_django_field(cls, f, default=widgets.Widget):
+        if f.get_internal_type() in ("JSONField",):
+            return JSONWidget
+        else:
+            return super().widget_from_django_field(f)
+
+
+class SoloModelResource(resources.ModelResource):
+    def export(self, queryset=None, *args, **kwargs):
+        model = self._meta.model
+        model.get_solo()
+        return super().export(queryset, *args, **kwargs)
+
+
+class ServiceResource(JSONResourceMixin, resources.ModelResource):
+    class Meta:
+        model = Service
+
+
+class URLRewriteResource(resources.ModelResource):
+    class Meta:
+        model = URLRewrite
+
+
+class CamundaConfigResource(SoloModelResource):
+    class Meta:
+        model = CamundaConfig
