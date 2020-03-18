0d2b5a02a2204b5326e2b49cd0957d9c2efecca5
Sergei Maertens <sergei@maykinmedia.nl>
2020-03-18 09:42:33 +0100

    :sparkles: Restrict zaaktype filter to currently-relevant zaaktypen

--- src/zac/core/forms.py
+++ src/zac/core/forms.py
@@ -1,20 +1,30 @@
 from django import forms
+from django.template.defaultfilters import date
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 from .services import get_zaaktypes
 
 
 def get_zaaktype_choices():
-    # TODO: sorting of zaaktypes
-    for zaaktype in get_zaaktypes():
-        yield (zaaktype.url, zaaktype.omschrijving)
+    today = timezone.now().date()
+    zaaktypen = get_zaaktypes()
+    for zaaktype in zaaktypen:
+        if zaaktype.begin_geldigheid > today:
+            continue
+
+        if zaaktype.einde_geldigheid and zaaktype.einde_geldigheid < today:
+            continue
+
+        label = f"{zaaktype.omschrijving} (versie {date(zaaktype.versiedatum)})"
+        yield (zaaktype.url, label)
 
 
 class ZakenFilterForm(forms.Form):
     identificatie = forms.CharField(label=_("identificatie"), required=False,)
     bronorganisatie = forms.CharField(label=_("bronorganisatie"), required=False,)
     zaaktypen = forms.MultipleChoiceField(
-        label=_("zaaktypen"),
+        label=_("zaaktypen (huidige versies)"),
         required=False,
         choices=get_zaaktype_choices,
         widget=forms.CheckboxSelectMultiple,
