36698c90ebf5ac8c9401ec6cd81cccf493403c68
Sergei Maertens <sergei@maykinmedia.nl>
2020-04-02 15:21:15 +0200

    :sparkles: Drive search options from backend

--- src/zac/core/views/search.py
+++ src/zac/core/views/search.py
@@ -1,19 +1,51 @@
+from dataclasses import dataclass
+from typing import List, Tuple, Type
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms import TextInput, Widget
 from django.views.generic import TemplateView
 
-from djchoices import ChoiceItem, DjangoChoices
+from zac.contrib.kadaster.forms import PandSelectieWidget
 
 
-class BagTypes(DjangoChoices):
-    pand = ChoiceItem("pand", "Pand")
-    address = ChoiceItem("address", "Adres (TODO)")
-    geometry = ChoiceItem("geometry", "Vrije geo-zoekopdracht (TODO)")
+@dataclass
+class ObjectType:
+    value: str
+    label: str
+    widget: Type[Widget]
+
+    def render_widget(self) -> str:
+        widget = self.widget()
+        return widget.render(name=self.value, value="")
+
+
+@dataclass
+class Registration:
+    label: str
+    object_types: List[ObjectType]
+
+    @property
+    def object_type_choices(self) -> List[Tuple[str, str]]:
+        return [
+            (object_type.value, object_type.label) for object_type in self.object_types
+        ]
 
 
 REGISTRATIONS = {
-    "bag": {"label": "BAG", "object_types": BagTypes.choices,},
-    "brp": {"label": "BRP", "object_types": [],},
-    "bgt_brt": {"label": "BGT/BRT", "object_types": [],},
+    "bag": Registration(
+        label="BAG",
+        object_types=[
+            ObjectType(value="pand", label="Pand", widget=PandSelectieWidget),
+            ObjectType(value="address", label="Adres (TODO)", widget=TextInput),
+            ObjectType(
+                value="geometry",
+                label="Vrije geo-zoekopdracht (TODO)",
+                widget=TextInput,
+            ),
+        ],
+    ),
+    "brp": Registration(label="BRP", object_types=[]),
+    "bgt_brt": Registration(label="BGT/BRT", object_types=[]),
 }
 
 
