e0949d7ccf675d5a1e0a3a211e24c236591183d9
Sergei Maertens <sergei@maykinmedia.nl>
2020-04-02 12:58:20 +0200

    :lipstick: Design search page

--- src/zac/core/views/search.py
+++ src/zac/core/views/search.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+from djchoices import ChoiceItem, DjangoChoices
+
+
+class BagTypes(DjangoChoices):
+    pand = ChoiceItem("pand", "Pand")
+    address = ChoiceItem("address", "Adres (TODO)")
+    geometry = ChoiceItem("geometry", "Vrije geo-zoekopdracht (TODO)")
+
+
+REGISTRATIONS = {
+    "bag": {"label": "BAG", "object_types": BagTypes.choices,},
+    "brp": {"label": "BRP", "object_types": [],},
+    "bgt_brt": {"label": "BGT/BRT", "object_types": [],},
+}
+
 
 class SearchIndexView(LoginRequiredMixin, TemplateView):
     template_name = "search/index.html"
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context.update({"registrations": REGISTRATIONS})
+        return context
