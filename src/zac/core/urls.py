bd319ef4b6b14212f24b9b96aca5cb70544d1cdf
Sergei Maertens <sergei@maykinmedia.nl>
2020-04-02 12:16:32 +0200

    :sparkles: Boilerplate for search

--- src/zac/core/urls.py
+++ src/zac/core/urls.py
@@ -1,33 +1,60 @@
-from django.urls import path
+from django.urls import include, path
 
 from .views.cache import FlushCacheView
 from .views.documents import DownloadDocumentView
 from .views.processes import ClaimTaskView, FetchTasks
+from .views.search import SearchIndexView
 from .views.zaken import FetchZaakObjecten, Index, ZaakAfhandelView, ZaakDetail
 
 app_name = "core"
 
 urlpatterns = [
-    path("", Index.as_view(), name="index"),
     path(
-        "zaken/<bronorganisatie>/<identificatie>/",
-        ZaakDetail.as_view(),
-        name="zaak-detail",
+        "zaken/",
+        include(
+            [
+                path("", Index.as_view(), name="index"),
+                path(
+                    "<bronorganisatie>/<identificatie>/",
+                    ZaakDetail.as_view(),
+                    name="zaak-detail",
+                ),
+                path(
+                    "<bronorganisatie>/<identificatie>/afhandelen/",
+                    ZaakAfhandelView.as_view(),
+                    name="zaak-afhandeling",
+                ),
+            ]
+        ),
     ),
     path(
-        "zaken/<bronorganisatie>/<identificatie>/afhandelen/",
-        ZaakAfhandelView.as_view(),
-        name="zaak-afhandeling",
+        "documenten/",
+        include(
+            [
+                path(
+                    "<bronorganisatie>/<identificatie>/",
+                    DownloadDocumentView.as_view(),
+                    name="download-document",
+                ),
+            ]
+        ),
     ),
     path(
-        "documenten/<bronorganisatie>/<identificatie>/",
-        DownloadDocumentView.as_view(),
-        name="download-document",
+        "search/", include([path("", SearchIndexView.as_view(), name="search-index"),])
     ),
     path(
-        "_fetch-zaakobjecten", FetchZaakObjecten.as_view(), name="fetch-zaakobjecten",
+        "_",
+        include(
+            [
+                path(
+                    "fetch-zaakobjecten",
+                    FetchZaakObjecten.as_view(),
+                    name="fetch-zaakobjecten",
+                ),
+                path("fetch-tasks", FetchTasks.as_view(), name="fetch-tasks"),
+                path("claim-task", ClaimTaskView.as_view(), name="claim-task"),
+                path("flush-cache/", FlushCacheView.as_view(), name="flush-cache"),
+            ]
+        ),
     ),
-    path("_fetch-tasks", FetchTasks.as_view(), name="fetch-tasks"),
-    path("_claim-task", ClaimTaskView.as_view(), name="claim-task"),
-    path("_flush-cache/", FlushCacheView.as_view(), name="flush-cache"),
 ]
