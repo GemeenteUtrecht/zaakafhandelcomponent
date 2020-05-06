f290a2d0ab30f71ffbec39650e2d6b9088065631
annashamray <anna.shamray@maykinmedia.nl>
2020-05-06 19:13:43 +0200

    add initial data to rendering checkbox in js

--- src/zac/js/components/checkbox-select-dynamic/index.js
+++ src/zac/js/components/checkbox-select-dynamic/index.js
@@ -1,29 +1,41 @@
 const SELECTOR = '.checkbox-select.checkbox-select--dynamic';
 
 
-const getOptionsHtml = (name, options) => {
+const getOptionsHtml = (name, options, selectedValues) => {
+    const valueSet = new Set(selectedValues);
     const htmlBits = options.map(([value, display], index) => {
         const id = `id_${name}_${index}`;
         return `
-        <li class="checkbox-select__option">
-            <input type="checkbox" name="${name}" id="${id}" value="${value}">
-            <label for="${id}">${display}</label>
-        </li>`;
+            <li class="checkbox-select__option">
+                <input
+                    type="checkbox"
+                    name="${name}"
+                    id="${id}"
+                    value="${value}"
+                    ${valueSet.has(value) ? 'checked' : ''}
+                >
+                <label for="${id}">${display}</label>
+            </li>`;
     });
     return htmlBits.join("\n");
 };
 
-
 const checkboxSelect = (node) => {
-    const { name, listen, values } = node.dataset;
+    const { name, listen, values, initial } = node.dataset;
     const options = JSON.parse(document.getElementById(values).innerText);
+    const initials = JSON.parse(document.getElementById(initial).innerText);
+    const showOptions = (group, values) => {
+        const markUp = getOptionsHtml(name, options[group] || [], [values]);
+        node.innerHTML = markUp;
+    };
 
-    document
-        .getElementById(listen)
-        .addEventListener('change', (e) => {
-            const markUp = getOptionsHtml(name, options[e.target.value]);
-            node.innerHTML = markUp;
-        });
+    const catalogus = document.getElementById(listen);
+
+    catalogus
+        .addEventListener('change', (e) => showOptions(e.target.value, []));
+
+    const selectedValue = (catalogus.querySelector('[checked]') || []).value;
+    showOptions(selectedValue, initials);
 };
 
 
