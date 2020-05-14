b0de7e8846e0a6731e9cd482f664f300a07db29a
Sergei Maertens <sergei@maykinmedia.nl>
2020-05-14 13:29:01 +0200

    :zap: Cache user permission codes for 5 minutes
    This reduces the amount of database connections, suspected to be spawned from the
    concurrent.futures worker threads.

--- src/zac/accounts/backends.py
+++ src/zac/accounts/backends.py
@@ -4,6 +4,8 @@ from django.contrib.auth.hashers import check_password
 
 import rules
 
+from zac.utils.decorators import cache
+
 from .datastructures import ZaakPermissionCollection
 
 
@@ -20,6 +22,14 @@ class UserModelEmailBackend(ModelBackend):
         except get_user_model().DoesNotExist:
             # No user was found, return None - triggers default login failed
             return None
+
+
+# TODO: invalidate cache on permission changes!
+@cache("user:permission-codes:{user.id}", timeout=60 * 5)
+def _get_user_permission_codes(user):
+    # inventory of non-object level permissions
+    qs = user.auth_profiles.values_list("permission_sets__permissions", flat=True)
+    return set(sum(qs, []))
 
 
 class PermissionsBackend:
@@ -41,10 +51,7 @@ class PermissionsBackend:
         self.set_user_permissions(user_obj)
 
         # inventory of non-object level permissions
-        qs = user_obj.auth_profiles.values_list(
-            "permission_sets__permissions", flat=True
-        )
-        permission_codes = set(sum(qs, []))
+        permission_codes = _get_user_permission_codes(user_obj)
         if not obj and perm not in permission_codes:
             if not rules.rule_exists(perm):
                 return False
