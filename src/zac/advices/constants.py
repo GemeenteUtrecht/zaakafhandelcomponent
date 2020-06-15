d1caff20d9a35a8ecf2085401598bb9de501cdaa
Sergei Maertens <sergei@maykinmedia.nl>
2020-06-15 12:09:22 +0200

    :fire: Delete code that's moved to Kownsl

--- src/zac/advices/constants.py
+++
@@ -1,8 +0,0 @@
-from django.utils.translation import gettext_lazy as _
-
-from djchoices import ChoiceItem, DjangoChoices
-
-
-class AdviceObjectTypes(DjangoChoices):
-    zaak = ChoiceItem("zaak", _("Zaak"))
-    document = ChoiceItem("document", _("Zaak"))
--- src/zac/advices/forms.py
+++ src/zac/advices/forms.py
@@ -3,37 +3,10 @@ from django import forms
 from django_camunda.api import get_process_instance_variable
 
 from zac.core.cache import invalid_zio_cache, invalidate_zaak_cache
-from zac.core.forms import TaskFormMixin, _repr
+from zac.core.forms import _repr
 from zac.core.services import get_document, get_documenten, get_zaak, update_document
 
-from .constants import AdviceObjectTypes
-from .models import Advice, DocumentAdvice
-
-
-class AdviceForm(TaskFormMixin, forms.ModelForm):
-    class Meta:
-        model = Advice
-        fields = (
-            "advice",
-            "accord",
-        )
-
-    def on_submission(self):
-        assert self.is_valid(), "Form must be validated first"
-        self.instance.user = self.context["request"].user
-
-        # fetch the zaak that it applies to
-        zaak_url = get_process_instance_variable(
-            self.task.process_instance_id, "zaakUrl"
-        )
-        self.instance.object_type = AdviceObjectTypes.zaak
-        self.instance.object_url = zaak_url
-
-        self.save()
-
-    def get_process_variables(self):
-        # does not set any variables
-        return {}
+from .models import DocumentAdvice
 
 
 class DocDescWidget(forms.TextInput):
@@ -72,7 +45,7 @@ class UploadDocumentBaseFormSet(forms.BaseFormSet):
 
         super().__init__(*args, **kwargs)
 
-    def on_submission(self, form: AdviceForm):
+    def on_submission(self, form):
         assert self.is_valid(), "FormSet must be validated first"
 
         advice = form.instance
