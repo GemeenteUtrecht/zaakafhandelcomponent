976629b0fa94c06d6c47c672e17f31c29db04548
Sergei Maertens <sergei@maykinmedia.nl>
2020-07-17 14:56:46 +0200

    :sparkles: Support displaying related 'omgevingsdossiers'

--- src/zac/core/zaakobjecten.py
+++ src/zac/core/zaakobjecten.py
@@ -7,6 +7,10 @@ from zgw_consumers.api_models.zaken import ZaakObject
 from zac.contrib.kadaster.bag import fetch_pand, fetch_verblijfsobject
 
 
+def noop(url: str) -> str:
+    return url
+
+
 @dataclass
 class ZaakObjectGroup:
     """
@@ -34,5 +38,11 @@ GROUPS = {
         label="Verblijfsobjecten",
         retriever=fetch_verblijfsobject,
         template="core/includes/zaakobjecten/verblijfsobject.html",
+    ),
+    # Roxxit Squit 20/20 POC
+    "omgevingsdossier": ZaakObjectGroup(
+        label="Omgevingsdossiers",
+        retriever=noop,
+        template="core/includes/zaakobjecten/omgevingsdossier.html",
     ),
 }
