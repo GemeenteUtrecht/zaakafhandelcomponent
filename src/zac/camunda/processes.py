66f15ec925b56d7feb7f875679e90afe02df86e2
annashamray <anna.shamray@maykinmedia.nl>
2020-07-31 15:05:04 +0200

    get process trees from camunda on selected zaak url

---
+++ src/zac/camunda/processes.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from django_camunda.client import get_client
+from django_camunda.types import CamundaId
+from zgw_consumers.api_models.base import Model, factory
+
+
+@dataclass
+class ProcessInstance(Model):
+    id: CamundaId
+    definition_id: str
+
+    sub_processes: list = field(default_factory=list)
+    parent_process: str = None
+    zaak_url: str = None
+
+
+def add_subprocesses(process_instance, process_instances, client):
+    if process_instance.sub_processes:
+        return
+
+    response = client.get(
+        "process-instance", {"superProcessInstance": process_instance.id}
+    )
+    # todo restrict for other zaakUrls
+    for data in response:
+        sub_process_instance = process_instances.get(
+            data["id"], factory(ProcessInstance, data)
+        )
+
+        sub_process_instance.parent_process = process_instance
+        process_instance.sub_processes.append(sub_process_instance)
+        process_instances[sub_process_instance.id] = sub_process_instance
+
+        add_subprocesses(sub_process_instance, process_instances, client)
+
+
+def get_process_instances(zaak_url: str) -> List[ProcessInstance]:
+    client = get_client()
+
+    #  get process-instances for particular zaak
+    response = client.get("process-instance", {"variables": f"zaakUrl_eq_{zaak_url}"})
+
+    process_instances = {}
+    for data in response:
+        process_instance = factory(ProcessInstance, data)
+        process_instance.zaak_url = zaak_url
+        process_instances[data["id"]] = process_instance
+
+    # fill in all subpocesses into the dict
+    #  todo concurrency?
+    for id, process_instance in process_instances.copy().items():
+        add_subprocesses(process_instance, process_instances, client)
+
+    #  display them prettily
+    top_level_processes = list(
+        filter(lambda x: not x.parent_process, process_instances.values())
+    )
+    return top_level_processes
