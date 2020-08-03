dbf77b4725339f378f0d56329c1408ecb1b9de5e
Sergei Maertens <sergei@maykinmedia.nl>
2020-08-03 11:16:29 +0200

    :lipstick: Layout of user tasks (primary and nested)

--- src/zac/js/components/processes/ProcessInteraction.js
+++ src/zac/js/components/processes/ProcessInteraction.js
@@ -28,10 +28,10 @@ const ProcessInteraction = ({zaak, endpoint, sendMessageUrl, canDoUsertasks=fals
 
                 {
                     !canDoUsertasks ? null : (
-                        <React.Fragment>
-                            <div className="user-tasks user-tasks--primary">
+                        <div className="user-tasks">
+                            <div className="user-tasks__task-list user-tasks__task-list--primary">
+                                <h2 className="user-tasks__title">Taken</h2>
 
-                                <h2>Taken</h2>
                                 <UserTask
                                     zaakUrl={zaak}
                                     taskId="proces:1:task:1"
@@ -53,10 +53,44 @@ const ProcessInteraction = ({zaak, endpoint, sendMessageUrl, canDoUsertasks=fals
 
                             </div>
 
-                            <div className="user-tasks user-tasks--nested">
-                                <h2>Deeltaken</h2>
+                            <div className="user-tasks__task-list user-tasks__task-list--nested">
+                                <h2 className="user-tasks__title">Deeltaken</h2>
+
+                                <div className="user-tasks__subprocess"> Sub 1 </div>
+
+                                <UserTask
+                                    zaakUrl={zaak}
+                                    taskId="proces:3:task:1"
+                                    taskUrl="/core/zaken/:org/:identificatie/proces:3:task:1"
+                                    name="Adviseren"
+                                    created="2020-07-30T15:03:21Z"
+                                    assignee={{username: 'johndoe', firstName: '', lastName: ''}}
+                                    hasForm={true}
+                                />
+
+                                <div className="user-tasks__subprocess"> Sub 2 &gt; Sub 3 </div>
+
+                                <UserTask
+                                    zaakUrl={zaak}
+                                    taskId="proces:4:task:1"
+                                    taskUrl="/core/zaken/:org/:identificatie/proces:4:task:1"
+                                    name="Beoordelen"
+                                    created="2020-07-30T15:03:21Z"
+                                    assignee={{username: 'johndoe', firstName: '', lastName: ''}}
+                                    hasForm={true}
+                                />
+
+                                <UserTask
+                                    zaakUrl={zaak}
+                                    taskId="proces:4:task:2"
+                                    taskUrl="/core/zaken/:org/:identificatie/proces:4:task:2"
+                                    name="Debugging"
+                                    created="2020-07-30T15:03:21Z"
+                                    hasForm={true}
+                                />
+
                             </div>
-                        </React.Fragment>
+                        </div>
                     )
                 }
 
--- src/zac/js/views/zaak-detail/utils.js
+++ src/zac/js/views/zaak-detail/utils.js
@@ -1,7 +1,7 @@
 const getAuthorName = (user) => {
     const lastName = user.last_name ?? user.lastName;
     const firstName = user.first_name ?? user.firstName;
-    return lastName ? `${firstName} ${lastName}` : author.username;
+    return lastName ? `${firstName} ${lastName}` : user.username;
 };
 
 export { getAuthorName };
