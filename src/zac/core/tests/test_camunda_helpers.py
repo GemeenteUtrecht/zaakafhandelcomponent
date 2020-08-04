92889c2ee90c11527ce0ca1eaea63ff954c15b18
Sergei Maertens <sergei@maykinmedia.nl>
2020-08-04 12:38:25 +0200

    :white_check_mark: Add tests for `get_process_zaak_url`
    Utility function to retrieve the zaakUrl process variable from the
    process instance (or its nearest parent) was untested so far.

---
+++ src/zac/core/tests/test_camunda_helpers.py
@@ -0,0 +1,62 @@
+from django.test import TestCase
+
+import requests_mock
+from django_camunda.utils import serialize_variable
+
+from zac.camunda.data import ProcessInstance
+
+from ..camunda import get_process_zaak_url
+
+PI_URL = "https://camunda.example.com/engine-rest/process-instance"
+
+
+@requests_mock.Mocker()
+class GetProcessZaakUrlTests(TestCase):
+    def test_variable_on_subprocess(self, m):
+        process = ProcessInstance(
+            id="aProcessInstanceId", definition_id="some-process:1"
+        )
+        m.get(
+            f"{PI_URL}/aProcessInstanceId/variables/zaakUrl?deserializeValues=false",
+            json=serialize_variable("dummy"),
+        )
+
+        zaak_url = get_process_zaak_url(process)
+
+        self.assertEqual(zaak_url, "dummy")
+
+    def test_variable_on_parent_process(self, m):
+        process = ProcessInstance(
+            id="aProcessInstanceId", definition_id="some-process:1"
+        )
+        m.get(
+            f"{PI_URL}/aProcessInstanceId/variables/zaakUrl?deserializeValues=false",
+            status_code=404,
+        )
+        m.get(
+            f"{PI_URL}?subProcessInstance=aProcessInstanceId",
+            json=[
+                {"id": "parentProcessInstanceId", "definitionId": "parent-process:1",}
+            ],
+        )
+        m.get(
+            f"{PI_URL}/parentProcessInstanceId/variables/zaakUrl?deserializeValues=false",
+            json=serialize_variable("dummy"),
+        )
+
+        zaak_url = get_process_zaak_url(process)
+
+        self.assertEqual(zaak_url, "dummy")
+
+    def test_variable_nowhere(self, m):
+        process = ProcessInstance(
+            id="aProcessInstanceId", definition_id="some-process:1"
+        )
+        m.get(
+            f"{PI_URL}/aProcessInstanceId/variables/zaakUrl?deserializeValues=false",
+            status_code=404,
+        )
+        m.get(f"{PI_URL}?subProcessInstance=aProcessInstanceId", json=[])
+
+        with self.assertRaises(RuntimeError):
+            get_process_zaak_url(process)
