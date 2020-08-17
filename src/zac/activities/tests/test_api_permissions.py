2c6ebacc66f68dfab9e117529f0b3ce325826824
Sergei Maertens <sergei@maykinmedia.nl>
2020-08-17 10:53:31 +0200

    :white_check_mark: Add a test for a user _with_ permissions

--- src/zac/activities/tests/test_api_permissions.py
+++ src/zac/activities/tests/test_api_permissions.py
@@ -3,9 +3,12 @@ from django.urls import reverse_lazy
 import requests_mock
 from rest_framework import status
 from rest_framework.test import APITestCase
+from zgw_consumers.api_models.constants import VertrouwelijkheidsAanduidingen
 from zgw_consumers.models import APITypes, Service
 
-from zac.accounts.tests.factories import UserFactory
+from zac.accounts.tests.factories import PermissionSetFactory, UserFactory
+from zac.core.permissions import zaken_inzien
+from zac.core.tests.utils import ClearCachesMixin
 from zac.tests.utils import generate_oas_component, mock_service_oas_get
 
 from .factories import ActivityFactory
@@ -14,7 +17,7 @@ ZAKEN_ROOT = "https://open-zaak.nl/zaken/api/v1/"
 CATALOGI_ROOT = "https://open-zaak.nl/catalogi/api/v1/"
 
 
-class ReadPermissionTests(APITestCase):
+class ReadPermissionTests(ClearCachesMixin, APITestCase):
     """
     Test the activity list endpoint permissions.
 
@@ -32,6 +35,9 @@ class ReadPermissionTests(APITestCase):
 
         Service.objects.create(
             label="Zaken API", api_type=APITypes.zrc, api_root=ZAKEN_ROOT
+        )
+        Service.objects.create(
+            label="Catalogi API", api_type=APITypes.ztc, api_root=CATALOGI_ROOT,
         )
 
     def test_read_not_logged_in(self):
@@ -65,3 +71,48 @@ class ReadPermissionTests(APITestCase):
         response = self.client.get(self.endpoint, {"zaak": zaak["url"]})
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @requests_mock.Mocker()
+    def test_read_logged_in_zaak_permission(self, m):
+        user = UserFactory.create()
+        self.client.force_login(user)
+        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
+        mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
+        catalogus = f"{CATALOGI_ROOT}/catalogussen/e13e72de-56ba-42b6-be36-5c280e9b30cd"
+        zaaktype = generate_oas_component(
+            "ztc",
+            "schemas/ZaakType",
+            catalogus=catalogus,
+            url=f"{CATALOGI_ROOT}zaaktypen/d66790b7-8b01-4005-a4ba-8fcf2a60f21d",
+            identificatie="ZT1",
+        )
+        m.get(
+            f"{CATALOGI_ROOT}zaaktypen",
+            json={"count": 1, "previous": None, "next": None, "results": [zaaktype],},
+        )
+        zaak = generate_oas_component(
+            "zrc",
+            "schemas/Zaak",
+            url=f"{ZAKEN_ROOT}zaken/30a98ef3-bf35-4287-ac9c-fed048619dd7",
+            zaaktype=zaaktype["url"],
+        )
+        m.get(zaak["url"], json=zaak)
+
+        # set up user permissions
+        PermissionSetFactory.create(
+            permissions=[zaken_inzien.name],
+            for_user=user,
+            catalogus=catalogus,
+            zaaktype_identificaties=["ZT1"],
+            max_va=VertrouwelijkheidsAanduidingen.zeer_geheim,
+        )
+        # set up test data
+        ActivityFactory.create()
+        activity = ActivityFactory.create(zaak=zaak["url"])
+
+        response = self.client.get(self.endpoint, {"zaak": zaak["url"]})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+        self.assertEqual(response.data[0]["id"], activity.id)
