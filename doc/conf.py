0d1695f544543db2e5a145a8452cd47941914db0
Sergei Maertens <sergei@maykinmedia.nl>
2020-09-07 16:43:54 +0200

    :bookmark: Bump to 0.20.0
    * Better looking select-documents widget
    * Added support for ValidSign package configuration
    * Added support for fetching extra BRP data (backend only)

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Maykin Media, Gemeente Utrecht"
 author = "Maykin Media, Gemeente Utrecht"
 
 # The full version, including alpha/beta/rc tags
-release = "0.19.0"
+release = "0.20.0"
 
 
 # -- General configuration ---------------------------------------------------
