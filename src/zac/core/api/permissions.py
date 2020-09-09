928a4c405d73b91ac4e9c37becaae7b3cf24dbc0
Sergei Maertens <sergei@maykinmedia.nl>
2020-09-09 12:10:11 +0200

    :white_check_mark: Add document upload permission tests

---
+++ src/zac/core/api/permissions.py
@@ -0,0 +1,35 @@
+import logging
+
+from rest_framework import permissions
+from rest_framework.request import Request
+from rest_framework.views import APIView
+from zds_client import ClientError
+
+from ..permissions import zaken_add_documents
+from ..services import get_zaak
+
+logger = logging.getLogger(__name__)
+
+
+class CanAddDocuments(permissions.BasePermission):
+    def _has_zaak_permission(self, request: Request, zaak_url: str):
+        # retrieve the zaak to check permissions for
+        try:
+            zaak = get_zaak(zaak_url=zaak_url)
+        except ClientError:
+            logger.info("Invalid Zaak specified", exc_info=True)
+            return False
+
+        return request.user.has_perm(zaken_add_documents.name, zaak)
+
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        if request.method != "POST":
+            return False
+
+        serializer = view.get_serializer(data=request.data)
+        # if the serializer is not valid, we want to see validation errors -> permission is granted
+        if not serializer.is_valid():
+            return True
+
+        zaak_url = serializer.validated_data["zaak"]
+        return self._has_zaak_permission(request, zaak_url)
