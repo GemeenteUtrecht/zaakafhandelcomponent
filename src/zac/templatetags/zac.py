738ccfc143e28f22d316441dfcd32fbd4ab01a2b
annashamray <anna.shamray@maykinmedia.nl>
2020-09-24 16:41:36 +0200

    :sparkles: https://github.com/GemeenteUtrecht/zaakafhandelcomponent/pull/70#pullrequestreview-487587999 -- show access requests counter on the side-nav

--- src/zac/templatetags/zac.py
+++ src/zac/templatetags/zac.py
@@ -4,12 +4,15 @@ from django.http import HttpRequest
 from django.shortcuts import resolve_url
 from django.template import Library
 
+from zac.accounts.models import AccessRequest
+from zac.core.services import find_zaak
+
 register = Library()
 
 
 @register.inclusion_tag("includes/nav-menu__item.html", takes_context=True)
 def nav_menu_item(
-    context: dict, target: str, label: str, *args, **kwargs
+    context: dict, target: str, label: str, count: int = None, *args, **kwargs
 ) -> Dict[str, Any]:
     target_url = resolve_url(target, *args, **kwargs)
 
@@ -20,4 +23,12 @@ def nav_menu_item(
         "label": label,
         "target_url": target_url,
         "is_active": is_active,
+        "count": count,
     }
+
+
+@register.simple_tag
+def count_access_requests(**kwargs):
+    """returns the number of active access requests for a zaak"""
+    zaak = find_zaak(**kwargs)
+    return AccessRequest.objects.filter(zaak=zaak.url, result="").count()
