7a653262ff5a98b3e947a781125df0e8752e1c50
Sergei Maertens <sergei@maykinmedia.nl>
2020-10-07 13:46:10 +0200

    :bookmark: Bump to 0.24.0
    * Tweaked temp access request
    * Refactored kownsl specific variables
    * Added hijack

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Maykin Media, Gemeente Utrecht"
 author = "Maykin Media, Gemeente Utrecht"
 
 # The full version, including alpha/beta/rc tags
-release = "0.23.1"
+release = "0.24.0"
 
 
 # -- General configuration ---------------------------------------------------
