42ae422acb7ef43e4b54741559d27ef49f56075f
Sergei Maertens <sergei@maykinmedia.nl>
2020-10-08 14:21:45 +0200

    :alien: Ensure ad-hoc activities are destroyed on zaak destroy

--- src/zac/notifications/views.py
+++ src/zac/notifications/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from zgw_consumers.api_models.base import factory
 from zgw_consumers.api_models.zaken import Zaak
 
+from zac.activities.models import Activity
 from zac.core.cache import invalidate_zaak_cache, invalidate_zaak_list_cache
 from zac.core.services import _client_from_url
 
@@ -26,7 +27,7 @@ class NotificationCallbackView(APIView):
                 self._handle_zaak_creation(data["hoofd_object"])
             elif data["actie"] in ["update", "partial_update"]:
                 self._handle_zaak_update(data["hoofd_object"])
-            elif data["actie"] == ["destroy"]:
+            elif data["actie"] == "destroy":
                 self._handle_zaak_destroy(data["hoofd_object"])
         elif (
             data["resource"] in ["resultaat", "status", "zaakeigenschap"]
@@ -50,9 +51,7 @@ class NotificationCallbackView(APIView):
         invalidate_zaak_list_cache(client, zaak)
 
     def _handle_zaak_destroy(self, zaak_url: str):
-        import bpdb
-
-        bpdb.set_trace()
+        Activity.objects.filter(zaak=zaak_url).delete()
 
     def _handle_related_creation(self, zaak_url):
         zaak = self._retrieve_zaak(zaak_url)
