5a22a8b1966d10f863a2463a992704969a680ea2
Kelvin Chan <kelvinchancy95@gmail.com>
2020-10-15 14:35:56 +0200

    Added delete step functionality

--- src/zac/js/components/review-user-select/UserSelect.js
+++ src/zac/js/components/review-user-select/UserSelect.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Select from 'react-select';
 import { getUserName } from '../../utils/users';
 import { HiddenCheckbox } from '../forms/Inputs';
+import DeleteButton from '../forms/DeleteButton';
 import { ErrorList, Wrapper } from '../forms/Utils';
 
 const setUsers = (users) => (users ? users.map((user) => ({
@@ -20,7 +21,7 @@ const isEmpty = (obj) => {
 const extractErrors = (errList) => errList.map((err) => err.msg);
 
 const UserSelect = ({
-    index, totalStepsIndex, data: { values, errors }, users,
+    index, totalStepsIndex, data: { values, errors }, users, onDelete,
 }) => {
     const [selectedData, setSelectedData] = useState(null);
     const [hiddenInputs, setHiddenInputs] = useState(null);
@@ -46,10 +47,8 @@ const UserSelect = ({
         getHiddenInputs(selectedData);
     }, [selectedData]);
 
-    const StepTitle = totalStepsIndex !== 0 ? <h3>{`Stap ${index + 1}`}</h3> : <h3> </h3>;
-
     return (
-        <div className="user-select" style={{ position: 'relative' }}>
+        <div className="user-select">
             { (errors && !isEmpty(errors.__all__))
                 ? (
                     <Wrapper errors={errors.__all__}>
@@ -57,18 +56,26 @@ const UserSelect = ({
                     </Wrapper>
                 ) : null}
 
-            {StepTitle}
+            <div className="user-select__title">
+                { (totalStepsIndex !== 0)
+                && <h3>{`Stap ${index + 1}`}</h3> }
+
+                { (index === totalStepsIndex && index !== 0)
+                && <DeleteButton onDelete={onDelete} /> }
+            </div>
             {hiddenInputs}
-            <Select
-                isMulti
-                cacheOptions
-                placeholder="Selecteer adviseur(s)"
-                name={`kownsl_users${-index}`}
-                options={setUsers(users)}
-                className="basic-multi-select"
-                classNamePrefix="select"
-                onChange={(value) => handleSelectChange(value)}
-            />
+            <div className="user-select__selector">
+                <Select
+                    isMulti
+                    cacheOptions
+                    placeholder="Selecteer adviseur(s)"
+                    name={`kownsl_users${-index}`}
+                    options={setUsers(users)}
+                    className="basic-multi-select"
+                    classNamePrefix="select"
+                    onChange={(value) => handleSelectChange(value)}
+                />
+            </div>
         </div>
     );
 };
@@ -78,6 +85,7 @@ UserSelect.propTypes = {
     totalStepsIndex: PropTypes.number,
     data: PropTypes.objectOf(PropTypes.object).isRequired,
     users: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onDelete: PropTypes.func,
 };
 
 export default UserSelect;
