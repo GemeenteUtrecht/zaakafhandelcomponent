04e1ad4c555ace642be1845d96823c3a67127998
silvia <silvia@maykinmedia.nl>
2020-10-16 16:03:08 +0200

    :sparkles: added a select input that works with formsets

--- src/zac/js/components/forms/Select.js
+++ src/zac/js/components/forms/Select.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
 
 import { Help } from './Help';
 import { Label } from './Label';
 import { ErrorList, Wrapper } from './Utils';
+import {PrefixContext} from "../formsets/context";
 
 
 const Select = ({
@@ -53,6 +54,74 @@ const Select = ({
     );
 };
 
+const FormSetSelect = ({
+    name,
+    label='',
+    required=false,
+    choices=[],
+    id='',
+    helpText='',
+    classes=null,
+    onChange,
+    disabled=false,
+    value='',
+    errors=[]
+}) => {
+    const classNames = classes ?? 'input__control input__control--select';
+    const options = choices.map(([value, label], index) => {
+        return (
+            <option key={index} value={value}>{label}</option>
+        );
+    });
+
+    const prefix = useContext(PrefixContext);
+    const prefixedName = prefix ? `${prefix}-${name}` : name;
+    const prefixedId = (id && prefix) ? `${prefix}-${id}` : id;
+
+    const select = (
+        <select
+            name={prefixedName}
+            id={prefixedId}
+            className={classNames}
+            disabled={!!disabled}
+            value={value}
+            onChange={(event) => {
+                if (onChange) {
+                    onChange(event);
+                }
+            }}
+        >
+            { options }
+        </select>
+    );
+
+    return (
+        <Wrapper errors={errors}>
+            <Label label={label} required={required} />
+            <Help helpText={helpText} />
+            <ErrorList errors={errors} />
+            { select }
+        </Wrapper>
+    );
+};
+
+FormSetSelect.propTypes = {
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    required: PropTypes.bool,
+    choices: PropTypes.arrayOf(
+        PropTypes.arrayOf(PropTypes.string),
+    ),
+    id: PropTypes.string,
+    helpText: PropTypes.string,
+    classes: PropTypes.string,
+    onChange: PropTypes.func,
+    disabled: PropTypes.bool,
+    value: PropTypes.string,
+    errors: PropTypes.arrayOf(PropTypes.string),
+};
+
+
 Select.propTypes = {
     name: PropTypes.string.isRequired,
     label: PropTypes.string,
@@ -70,4 +139,4 @@ Select.propTypes = {
 };
 
 
-export { Select };
+export { Select, FormSetSelect };
