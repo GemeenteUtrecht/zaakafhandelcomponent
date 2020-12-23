ed5a07baed856821682369e0b18ba47ae54e7eb8
Sergei Maertens <sergei@maykinmedia.nl>
2020-12-23 11:14:23 +0100

    :recycle: Group API URLs together under common /api prefix

--- backend/src/zac/api/urls.py
+++ backend/src/zac/api/urls.py
@@ -7,7 +7,12 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
+    # API schema documentation
     path("", SpectacularAPIView.as_view(), name="api-schema-json"),
     path("schema", SpectacularJSONAPIView.as_view(), name="api-schema"),
     path("docs/", SpectacularRedocView.as_view(url_name="api-schema"), name="api-docs"),
+    # actual API endpoints
+    path("kownsl/", include("zac.contrib.kownsl.urls")),
+    path("core/", include("zac.core.api.bff_urls")),
+    path("", include("zac.notifications.urls")),
 ]
