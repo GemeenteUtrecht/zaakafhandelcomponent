327676bc5b5ac44a78ac8f1ef6fb54fdfca7e1c8
Sergei Maertens <sergei@maykinmedia.nl>
2021-01-04 15:33:47 +0100

    :white_check_mark: Added minimal tests for zaak-documents endpoint

--- backend/src/zac/core/api/tests/test_zaak_documents.py
+++ backend/src/zac/core/api/tests/test_zaak_documents.py
@@ -7,8 +7,9 @@ import requests_mock
 from rest_framework import status
 from rest_framework.test import APITestCase
 from zgw_consumers.api_models.base import factory
-from zgw_consumers.api_models.catalogi import ZaakType
+from zgw_consumers.api_models.catalogi import InformatieObjectType, ZaakType
 from zgw_consumers.api_models.constants import VertrouwelijkheidsAanduidingen
+from zgw_consumers.api_models.documenten import Document
 from zgw_consumers.constants import APITypes
 from zgw_consumers.models import Service
 from zgw_consumers.test import generate_oas_component, mock_service_oas_get
@@ -20,15 +21,16 @@ from zac.accounts.tests.factories import (
 )
 from zac.core.permissions import zaken_inzien
 from zac.core.tests.utils import ClearCachesMixin
-from zac.tests.utils import mock_resource_get, paginated_response
+from zac.tests.utils import paginated_response
 from zgw.models.zrc import Zaak
 
 CATALOGI_ROOT = "http://catalogus.nl/api/v1/"
 ZAKEN_ROOT = "http://zaken.nl/api/v1/"
+DOCUMENTS_ROOT = "http://documents.nl/api/v1/"
 
 
 @requests_mock.Mocker()
-class ZaakEigenschappenResponseTests(ClearCachesMixin, APITestCase):
+class ZaakDocumentsResponseTests(APITestCase):
     """
     Test the API response body for zaak-detail endpoint.
     """
@@ -41,6 +43,7 @@ class ZaakEigenschappenResponseTests(ClearCachesMixin, APITestCase):
 
         Service.objects.create(api_type=APITypes.ztc, api_root=CATALOGI_ROOT)
         Service.objects.create(api_type=APITypes.zrc, api_root=ZAKEN_ROOT)
+        Service.objects.create(api_type=APITypes.drc, api_root=DOCUMENTS_ROOT)
 
         catalogus_url = (
             f"{CATALOGI_ROOT}/catalogussen/e13e72de-56ba-42b6-be36-5c280e9b30cd"
@@ -53,18 +56,13 @@ class ZaakEigenschappenResponseTests(ClearCachesMixin, APITestCase):
             catalogus=catalogus_url,
             vertrouwelijkheidaanduiding=VertrouwelijkheidsAanduidingen.openbaar,
         )
-        cls.eigenschap = generate_oas_component(
+        cls.documenttype = generate_oas_component(
             "ztc",
-            "schemas/Eigenschap",
-            zaaktype=cls.zaaktype["url"],
-            naam="some-property",
-            specificatie={
-                "groep": "dummy",
-                "formaat": "tekst",
-                "lengte": "3",
-                "kardinaliteit": "1",
-                "waardenverzameling": [],
-            },
+            "schemas/InformatieObjectType",
+            url=f"{CATALOGI_ROOT}informatieobjecttypen/d5d7285d-ce95-4f9e-a36f-181f1c642aa6",
+            omschrijving="bijlage",
+            catalogus=catalogus_url,
+            vertrouwelijkheidaanduiding=VertrouwelijkheidsAanduidingen.openbaar,
         )
         cls.zaak = generate_oas_component(
             "zrc",
@@ -77,14 +75,33 @@ class ZaakEigenschappenResponseTests(ClearCachesMixin, APITestCase):
             startdatum="2020-12-25",
             uiterlijkeEinddatumAfdoening="2021-01-04",
         )
+        cls.document = generate_oas_component(
+            "drc",
+            "schemas/EnkelvoudigInformatieObject",
+            url=f"{DOCUMENTS_ROOT}enkelvoudiginformatieobjecten/0c47fe5e-4fe1-4781-8583-168e0730c9b6",
+            identificatie="DOC-2020-007",
+            bronorganisatie="123456782",
+            informatieobjecttype=cls.documenttype["url"],
+            vertrouwelijkheidaanduiding=VertrouwelijkheidsAanduidingen.openbaar,
+            bestandsomvang=10,
+        )
 
         zaak = factory(Zaak, cls.zaak)
         zaak.zaaktype = factory(ZaakType, cls.zaaktype)
 
+        document = factory(Document, cls.document)
+        document.informatieobjecttype = factory(InformatieObjectType, cls.documenttype)
+
         cls.find_zaak_patcher = patch("zac.core.api.views.find_zaak", return_value=zaak)
+        cls.get_review_requests_patcher = patch(
+            "zac.core.api.views.get_review_requests", return_value=[]
+        )
+        cls.get_documenten_patcher = patch(
+            "zac.core.api.views.get_documenten", return_value=([document], [])
+        )
 
         cls.endpoint = reverse(
-            "zaak-properties",
+            "zaak-documents",
             kwargs={
                 "bronorganisatie": "123456782",
                 "identificatie": "ZAAK-2020-0010",
@@ -97,55 +114,50 @@ class ZaakEigenschappenResponseTests(ClearCachesMixin, APITestCase):
         self.find_zaak_patcher.start()
         self.addCleanup(self.find_zaak_patcher.stop)
 
+        self.get_review_requests_patcher.start()
+        self.addCleanup(self.get_review_requests_patcher.stop)
+
+        self.get_documenten_patcher.start()
+        self.addCleanup(self.get_documenten_patcher.stop)
+
         # ensure that we have a user with all permissions
         self.client.force_authenticate(user=self.user)
 
-    def test_get_zaak_eigenschappen(self, m):
-        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
-        mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
-        mock_resource_get(m, self.zaak)
-        mock_resource_get(m, self.zaaktype)
-        m.get(
-            f"{CATALOGI_ROOT}eigenschappen?zaaktype={self.zaaktype['url']}",
-            json=paginated_response([self.eigenschap]),
+    def test_get_zaak_documents(self, m):
+        download_url = reverse(
+            "core:download-document",
+            kwargs={
+                "bronorganisatie": "123456782",
+                "identificatie": "DOC-2020-007",
+            },
         )
-        eigenschap = generate_oas_component(
-            "zrc",
-            "schemas/ZaakEigenschap",
-            zaak=self.zaak["url"],
-            eigenschap=self.eigenschap["url"],
-            naam=self.eigenschap["naam"],
-            waarde="bar",
-        )
-        m.get(f"{self.zaak['url']}/zaakeigenschappen", json=[eigenschap])
 
         response = self.client.get(self.endpoint)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_data = response.json()
-
         expected = [
             {
-                "url": eigenschap["url"],
-                "value": "bar",
-                "eigenschap": {
-                    "url": self.eigenschap["url"],
-                    "naam": "some-property",
-                    "toelichting": self.eigenschap["toelichting"],
-                    "specificatie": {
-                        "groep": "dummy",
-                        "formaat": "tekst",
-                        "lengte": "3",
-                        "kardinaliteit": "1",
-                        "waardenverzameling": [],
-                    },
+                "url": self.document["url"],
+                "auteur": self.document["auteur"],
+                "identificatie": "DOC-2020-007",
+                "beschrijving": self.document["beschrijving"],
+                "bestandsnaam": self.document["bestandsnaam"],
+                "locked": False,
+                "informatieobjecttype": {
+                    "url": f"{CATALOGI_ROOT}informatieobjecttypen/d5d7285d-ce95-4f9e-a36f-181f1c642aa6",
+                    "omschrijving": "bijlage",
                 },
+                "titel": self.document["titel"],
+                "vertrouwelijkheidaanduiding": "Openbaar",
+                "bestandsomvang": 10,
+                "downloadUrl": f"http://testserver{download_url}",
             }
         ]
         self.assertEqual(response_data, expected)
 
-    def test_no_properties(self, m):
-        with patch("zac.core.api.views.get_zaak_eigenschappen", return_value=[]):
+    def test_no_documents(self, m):
+        with patch("zac.core.api.views.get_documenten", return_value=[[], []]):
             response = self.client.get(self.endpoint)
 
         self.assertEqual(response.data, [])
@@ -157,7 +169,7 @@ class ZaakEigenschappenResponseTests(ClearCachesMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
-class ZaakPropertiesPermissionTests(ClearCachesMixin, APITestCase):
+class ZaakDocumentsPermissionTests(ClearCachesMixin, APITestCase):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
@@ -192,12 +204,15 @@ class ZaakPropertiesPermissionTests(ClearCachesMixin, APITestCase):
         zaak.zaaktype = factory(ZaakType, cls.zaaktype)
 
         cls.find_zaak_patcher = patch("zac.core.api.views.find_zaak", return_value=zaak)
-        cls.get_eigenschappen_patcher = patch(
-            "zac.core.api.views.get_zaak_eigenschappen", return_value=[]
+        cls.get_review_requests_patcher = patch(
+            "zac.core.api.views.get_review_requests", return_value=[]
+        )
+        cls.get_documenten_patcher = patch(
+            "zac.core.api.views.get_documenten", return_value=([], [])
         )
 
         cls.endpoint = reverse(
-            "zaak-properties",
+            "zaak-documents",
             kwargs={
                 "bronorganisatie": "123456782",
                 "identificatie": "ZAAK-2020-0010",
@@ -210,8 +225,11 @@ class ZaakPropertiesPermissionTests(ClearCachesMixin, APITestCase):
         self.find_zaak_patcher.start()
         self.addCleanup(self.find_zaak_patcher.stop)
 
-        self.get_eigenschappen_patcher.start()
-        self.addCleanup(self.get_eigenschappen_patcher.stop)
+        self.get_review_requests_patcher.start()
+        self.addCleanup(self.get_review_requests_patcher.stop)
+
+        self.get_documenten_patcher.start()
+        self.addCleanup(self.get_documenten_patcher.stop)
 
     def test_not_authenticated(self):
         response = self.client.get(self.endpoint)
