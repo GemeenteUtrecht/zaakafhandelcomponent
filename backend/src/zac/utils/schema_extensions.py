4fed43f75a0bab6732777447700d0e3175714b21
Sergei Maertens <sergei@maykinmedia.nl>
2021-02-10 11:42:31 +0100

    :sparkles: Fix schema-generation for custom filter backend
    No more warnings given for serializer fields, and schema
    generation is now using drf-spectacular built-ins.

--- backend/src/zac/utils/schema_extensions.py
+++ backend/src/zac/utils/schema_extensions.py
@@ -1,5 +1,5 @@
 from drf_spectacular.extensions import OpenApiFilterExtension
-from drf_spectacular.plumbing import build_basic_type, build_parameter_type
+from drf_spectacular.plumbing import build_parameter_type
 from drf_spectacular.utils import OpenApiParameter
 from rest_framework import fields
 
@@ -17,13 +17,14 @@ class ApiFilterExtension(OpenApiFilterExtension):
         parameters = []
         for field_name, field in filterset_class._filters.items():
             enum = field.choices if isinstance(field, fields.ChoiceField) else []
+            parameter_schema = auto_schema._map_serializer_field(field, "request")
             parameters.append(
                 build_parameter_type(
                     name=field_name,
                     required=field.required,
                     location=OpenApiParameter.QUERY,
                     description=field.help_text if field.help_text else field_name,
-                    schema=build_basic_type(field),
+                    schema=parameter_schema,
                     enum=enum,
                 )
             )
