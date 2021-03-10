80fb03255721f88e969e438986694c6f6d5ddd73
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-03-10 18:16:16 +0100

    :sparkles: #226 - add a permission mixin based on PermissionDefinition

--- backend/src/zac/api/permissions.py
+++ backend/src/zac/api/permissions.py
@@ -7,6 +7,8 @@ from rest_framework.request import Request
 from rest_framework.views import APIView
 from zds_client import ClientError
 
+from zac.accounts.constants import PermissionObjectType
+from zac.accounts.models import PermissionDefinition
 from zac.core.permissions import Permission
 from zac.core.services import get_zaak
 
@@ -65,3 +67,38 @@ class ZaakBasedPermission(permissions.BasePermission):
 
         zaak_url = serializer.validated_data[self.zaak_attr]
         return self._has_zaak_permission(request, zaak_url)
+
+
+class DefinitionBasePermission(permissions.BasePermission):
+    permission: Permission
+    object_type: str = PermissionObjectType.zaak
+
+    def __new__(cls, *args, **kwargs):
+        permission = getattr(cls, "permission", None)
+        if permission is None:
+            raise ImproperlyConfigured(
+                "%s is missing the 'permission' attribute" % cls.__name__
+            )
+        return super().__new__(cls, *args, **kwargs)
+
+    def has_object_permission(self, request, view, obj):
+        permission_definitions = (
+            PermissionDefinition.objects.for_user(request.user)
+            .actual()
+            .filter(permission=self.permission)
+        )
+
+        # first check atomic permissions
+        if permission_definitions.filter(object_url=obj.url).exists():
+            return True
+
+        # then check blueprint permissions
+        for permission in (
+            permission_definitions.filter(object_type=self.object_type)
+            .exclude(policy={})
+            .all()
+        ):
+            if permission.has_policy_access(obj, request):
+                return True
+
+        return False
