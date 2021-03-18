c57af51d121360dce2f78db6105aab06ed888b88
Kelvin Chan <kelvinchancy95@gmail.com>
2021-03-18 17:47:21 +0100

    Added autorisatieprofielen

--- frontend/zac-ui/apps/zac-ui/src/app/constants/menu.ts
+++ frontend/zac-ui/apps/zac-ui/src/app/constants/menu.ts
@@ -24,11 +24,20 @@ const bottomMenuItems: MenuItem[] = [
     icon: 'login',
     label: 'Inloggen',
     to: '/accounts/login/?next=/ui/',
+    external: true
+  },
+  {
+    icon: 'admin_panel_settings',
+    label: 'Autorisatieprofielen',
+    to: '/accounts/auth-profiles/',
+    external: true,
+    // roles: [UserRole.Admin],
   },
   {
     icon: 'admin',
     label: 'Admin',
     to: '/admin',
+    external: true
     // roles: [UserRole.Admin],
   },
 ];
--- frontend/zac-ui/libs/shared/data-access/models/ui/menu.ts
+++ frontend/zac-ui/libs/shared/data-access/models/ui/menu.ts
@@ -3,6 +3,7 @@ export interface MenuItem {
   icon?: string;
   label: string;
   to: string;
+  external?: boolean;
   subs?: MenuItem[];
   marginBottom?: boolean;
 }
