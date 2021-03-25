118c776d21213c6034d549409b4cc1f8ceaf7783
Kelvin Chan <kelvinchancy95@gmail.com>
2021-03-25 11:41:20 +0100

    Added auth interceptor

--- frontend/zac-ui/apps/zac-ui/src/app/app.module.ts
+++ frontend/zac-ui/apps/zac-ui/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LOCALE_ID } from '@angular/core';
 import localeNL from '@angular/common/locales/nl';
+
+import { AuthInterceptor } from './helpers/auth.interceptor';
 
 import { SharedUiComponentsModule } from '@gu/components';
 
@@ -16,7 +18,7 @@ import { HomeComponent } from './components/home/home.component';
 import { KownslModule } from './components/kownsl/kownsl.module';
 import { ZakenModule } from './components/zaken/zaken.module';
 import { WorkstackModule } from './components/workstack/workstack.module';
-import { ZakenComponent } from './components/zaken/zaken.component';
+
 
 registerLocaleData(localeNL);
 
@@ -40,6 +42,7 @@ registerLocaleData(localeNL);
     WorkstackModule
   ],
   providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: false},
     { provide: LOCALE_ID, useValue: "nl-NL" }
   ],
   bootstrap: [AppComponent],
---
+++ frontend/zac-ui/apps/zac-ui/src/app/helpers/auth.interceptor.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  readonly NOT_LOGGED_IN_MESSAGE = "Authenticatiegegevens zijn niet opgegeven.";
+
+  constructor(private router: Router) { }
+
+  private checkAuthentication(error: HttpErrorResponse): boolean {
+    return (error.status && error.status === 403 && error.error.detail === this.NOT_LOGGED_IN_MESSAGE);
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req)
+      .pipe(
+        catchError((error, caught) => {
+          if (error instanceof HttpErrorResponse) {
+            if (this.checkAuthentication(error)) {
+              const currentPath = this.router.url;
+              window.location.href = `/accounts/login/?next=/ui${currentPath}`;
+            } else {
+              return throwError(error);
+            }
+          }
+          return caught;
+        })
+      )
+  }
+}
