a2590d07759e330a95c58afdeb66cccbfba9694c
Daniel Ammeraal <danielammeraal@gmail.com>
2021-03-25 12:52:15 +0100

    Fix in polymorphic serializer

--- backend/src/zac/api/polymorphism.py
+++ backend/src/zac/api/polymorphism.py
@@ -92,7 +92,7 @@ class PolymorphicSerializer(serializers.Serializer):
 
     def is_valid(self, *args, **kwargs):
         valid = super().is_valid(*args, **kwargs)
-        extra_serializer = self._get_serializer_from_data(self.validated_data)
+        extra_serializer = self._get_serializer_from_data(self.data)
         extra_valid = extra_serializer.is_valid(*args, **kwargs)
         self._errors.update(extra_serializer.errors)
         return valid and extra_valid
