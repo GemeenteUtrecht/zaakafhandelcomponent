a060d4579baa7c44fef0317480abcc715f103522
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-03-25 18:19:32 +0100

    :fire: remove forgotten permission mixin

--- backend/src/zac/api/permissions.py
+++ backend/src/zac/api/permissions.py
@@ -15,38 +15,6 @@ from zac.core.services import get_zaak
 logger = logging.getLogger(__name__)
 
 
-class ZaakBasedPermission(permissions.BasePermission):
-    permission: Permission
-    zaak_attr = "zaak"
-
-    def __new__(cls, *args, **kwargs):
-        permission = getattr(cls, "permission", None)
-        if permission is None:
-            raise ImproperlyConfigured(
-                "%s is missing the 'permission' attribute" % cls.__name__
-            )
-        return super().__new__(cls, *args, **kwargs)
-
-    def _has_zaak_permission(self, request: Request, zaak_url: str):
-        # retrieve the zaak to check permissions for
-        try:
-            zaak = get_zaak(zaak_url=zaak_url)
-        except ClientError:
-            logger.info("Invalid Zaak specified", exc_info=True)
-            return False
-
-        return request.user.has_perm(self.permission.name, zaak)
-
-    def has_permission(self, request: Request, view: APIView) -> bool:
-        serializer = view.get_serializer(data=request.data)
-        # if the serializer is not valid, we want to see validation errors -> permission is granted
-        if not serializer.is_valid():
-            return True
-
-        zaak_url = serializer.validated_data[self.zaak_attr]
-        return self._has_zaak_permission(request, zaak_url)
-
-
 class DefinitionBasePermission(permissions.BasePermission):
     permission: Permission
     object_type: str = PermissionObjectType.zaak
