910caa4be9947d780d6ee6056a2a31e683b22d0d
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-04-14 17:16:19 +0200

    :white_check_mark: test refactored permissions for activity endpoints

--- backend/src/zac/activities/tests/test_api_permissions.py
+++ backend/src/zac/activities/tests/test_api_permissions.py
@@ -1,5 +1,3 @@
-from unittest.mock import patch
-
 from django.urls import reverse, reverse_lazy
 
 import requests_mock
@@ -19,7 +17,7 @@ ZAKEN_ROOT = "https://open-zaak.nl/zaken/api/v1/"
 CATALOGI_ROOT = "https://open-zaak.nl/catalogi/api/v1/"
 
 
-class ReadPermissionTests(ClearCachesMixin, APITestCase):
+class ListActivitiesPermissionTests(ClearCachesMixin, APITestCase):
     """
     Test the activity list endpoint permissions.
 
@@ -77,6 +75,45 @@ class ReadPermissionTests(ClearCachesMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     @requests_mock.Mocker()
+    def test_read_logged_in_permissions_for_other_zaak(self, m):
+        user = UserFactory.create()
+        self.client.force_authenticate(user)
+        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
+        mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
+        catalogus = f"{CATALOGI_ROOT}/catalogussen/e13e72de-56ba-42b6-be36-5c280e9b30cd"
+        zaaktype = generate_oas_component(
+            "ztc",
+            "schemas/ZaakType",
+            catalogus=catalogus,
+            url=f"{CATALOGI_ROOT}zaaktypen/d66790b7-8b01-4005-a4ba-8fcf2a60f21d",
+            identificatie="ZT1",
+            omschrijving="ZT1",
+        )
+        m.get(zaaktype["url"], json=zaaktype)
+        zaak = generate_oas_component(
+            "zrc",
+            "schemas/Zaak",
+            url=f"{ZAKEN_ROOT}zaken/30a98ef3-bf35-4287-ac9c-fed048619dd7",
+            zaaktype=zaaktype["url"],
+        )
+        m.get(zaak["url"], json=zaak)
+
+        # set up user permissions
+        BlueprintPermissionFactory.create(
+            permission=activities_read.name,
+            for_user=user,
+            policy={
+                "catalogus": catalogus,
+                "zaaktype_omschrijving": "ZT2",
+                "max_va": VertrouwelijkheidsAanduidingen.zeer_geheim,
+            },
+        )
+
+        response = self.client.get(self.endpoint, {"zaak": zaak["url"]})
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @requests_mock.Mocker()
     def test_read_logged_in_zaak_permission(self, m):
         user = UserFactory.create()
         self.client.force_authenticate(user)
@@ -122,7 +159,7 @@ class ReadPermissionTests(ClearCachesMixin, APITestCase):
         self.assertEqual(response.data[0]["id"], activity.id)
 
 
-class DetailReadPermissionTests(ClearCachesMixin, APITestCase):
+class ReadActivityDetailPermissionTests(ClearCachesMixin, APITestCase):
     """
     Test the activity detail endpoint permissions.
     """
@@ -181,6 +218,31 @@ class DetailReadPermissionTests(ClearCachesMixin, APITestCase):
         self.client.force_authenticate(self.user)
         mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
         mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
+        m.get(self.zaak["url"], json=self.zaak)
+
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @requests_mock.Mocker()
+    def test_read_logged_in_with_permissions_for_another_zaak(self, m):
+        endpoint = reverse(
+            "activities:activity-detail", kwargs={"pk": self.activity.pk}
+        )
+        self.client.force_authenticate(self.user)
+        # set up user permissions
+        BlueprintPermissionFactory.create(
+            permission=activities_read.name,
+            for_user=self.user,
+            policy={
+                "catalogus": self.catalogus,
+                "zaaktype_omschrijving": "ZT2",
+                "max_va": VertrouwelijkheidsAanduidingen.zeer_geheim,
+            },
+        )
+        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
+        mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
+        m.get(self.zaaktype["url"], json=self.zaaktype)
         m.get(self.zaak["url"], json=self.zaak)
 
         response = self.client.get(endpoint)
@@ -273,6 +335,33 @@ class CreatePermissionTests(ClearCachesMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     @requests_mock.Mocker()
+    def test_create_activity_logged_in_with_permissions_for_other_zaak(self, m):
+        self.client.force_authenticate(user=self.user)
+        # set up user permissions
+        BlueprintPermissionFactory.create(
+            permission=activiteiten_schrijven.name,
+            for_user=self.user,
+            policy={
+                "catalogus": self.catalogus,
+                "zaaktype_omschrijving": "ZT2",
+                "max_va": VertrouwelijkheidsAanduidingen.zeer_geheim,
+            },
+        )
+
+        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
+        mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
+        m.get(self.zaaktype["url"], json=self.zaaktype)
+        m.get(self.zaak["url"], json=self.zaak)
+        data = {
+            "zaak": self.zaak["url"],
+            "name": "Dummy",
+        }
+
+        response = self.client.post(self.endpoint, data)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @requests_mock.Mocker()
     def test_create_activity_logged_in_with_permissions(self, m):
         self.client.force_authenticate(user=self.user)
         # set up user permissions
@@ -313,6 +402,35 @@ class CreatePermissionTests(ClearCachesMixin, APITestCase):
         self.client.force_authenticate(user=self.user)
         mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
         mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
+        m.get(self.zaak["url"], json=self.zaak)
+        data = {
+            "activity": activity.id,
+            "notes": "Test notes",
+        }
+
+        response = self.client.post(endpoint, data)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @requests_mock.Mocker()
+    def test_create_event_logged_in_with_permissions_for_other_zaak(self, m):
+        endpoint = reverse_lazy("activities:event-list")
+        activity = ActivityFactory.create(zaak=self.zaak["url"])
+        self.client.force_authenticate(user=self.user)
+        # set up user permissions
+        BlueprintPermissionFactory.create(
+            permission=activiteiten_schrijven.name,
+            for_user=self.user,
+            policy={
+                "catalogus": self.catalogus,
+                "zaaktype_omschrijving": "ZT2",
+                "max_va": VertrouwelijkheidsAanduidingen.zeer_geheim,
+            },
+        )
+
+        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
+        mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
+        m.get(self.zaaktype["url"], json=self.zaaktype)
         m.get(self.zaak["url"], json=self.zaak)
         data = {
             "activity": activity.id,
@@ -408,6 +526,32 @@ class UpdatePermissionTests(ClearCachesMixin, APITestCase):
         self.client.force_authenticate(user=self.user)
         mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
         mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
+        m.get(self.zaak["url"], json=self.zaak)
+        data = {"name": "New name"}
+
+        response = self.client.patch(endpoint, data)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @requests_mock.Mocker()
+    def test_update_activity_logged_in_with_permissions_for_other_zaak(self, m):
+        endpoint = reverse(
+            "activities:activity-detail", kwargs={"pk": self.activity.pk}
+        )
+        self.client.force_authenticate(user=self.user)
+        # set up user permissions
+        BlueprintPermissionFactory.create(
+            permission=activiteiten_schrijven.name,
+            for_user=self.user,
+            policy={
+                "catalogus": self.catalogus,
+                "zaaktype_omschrijving": "ZT2",
+                "max_va": VertrouwelijkheidsAanduidingen.zeer_geheim,
+            },
+        )
+        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
+        mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
+        m.get(self.zaaktype["url"], json=self.zaaktype)
         m.get(self.zaak["url"], json=self.zaak)
         data = {"name": "New name"}
 
