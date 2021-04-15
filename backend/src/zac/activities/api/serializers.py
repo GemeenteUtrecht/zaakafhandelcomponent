d8aa89414b3219ed96435d93cb780c19e1dd932f
Daniel Ammeraal <danielammeraal@gmail.com>
2021-04-15 16:56:33 +0200

    Adapted Activity View for BFF

--- backend/src/zac/activities/api/serializers.py
+++ backend/src/zac/activities/api/serializers.py
@@ -1,6 +1,11 @@
+from django.utils.translation import gettext_lazy as _
+
 from rest_framework import serializers
 
-from zac.accounts.models import User
+from backend.src.zac.camunda.api.validators import UserValidator
+from zac.accounts.api.serializers import UserSerializer
+from zac.camunda.api.validators import UserValidator
+from zac.core.camunda.select_documents import DocumentSerializer
 
 from ..models import Activity, Event
 
@@ -16,13 +21,10 @@ class EventSerializer(serializers.ModelSerializer):
         )
 
 
-class ActivitySerializer(serializers.HyperlinkedModelSerializer):
-    assignee = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.filter(is_active=True),
-        required=False,
-        allow_null=True,
-    )
+class ActivitySerializer(serializers.ModelSerializer):
+    assignee = UserSerializer()
     events = EventSerializer(many=True, read_only=True)
+    document = DocumentSerializer(read_only=True)
 
     class Meta:
         model = Activity
@@ -43,3 +45,24 @@ class ActivitySerializer(serializers.HyperlinkedModelSerializer):
                 "view_name": "activities:activity-detail",
             },
         }
+
+
+class PatchActivitySerializer(ActivitySerializer):
+    assignee = serializers.CharField(
+        label=_("assignee"),
+        help_text=_("User assigned to the activity."),
+        allow_blank=True,
+        validators=(UserValidator(),),
+    )
+    document = serializers.URLField(
+        label=_("activity document url"),
+        help_text=_("URL that points to the document."),
+        allow_blank=True,
+    )
+
+    class Meta(ActivitySerializer.Meta):
+        fields = (
+            "assignee",
+            "document",
+            "status",
+        )
