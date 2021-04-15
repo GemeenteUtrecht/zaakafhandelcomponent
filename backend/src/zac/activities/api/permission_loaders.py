14251d7bb06971a37a6a24745234a71c855e1614
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-04-15 09:46:28 +0200

    :sparkles: grant permissions to the assignee of adhoc activities

---
+++ backend/src/zac/activities/api/permission_loaders.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from zac.accounts.models import AtomicPermission
+from zac.accounts.permission_loaders import add_atomic_permission_to_user
+
+from ..models import Activity
+from ..permissions import activiteiten_schrijven, activities_read
+
+
+def add_permissions_for_activity_assignee(activity: Activity) -> List[AtomicPermission]:
+    atomic_permissions = []
+    for permission in [activities_read, activiteiten_schrijven]:
+        atomic_permission = add_atomic_permission_to_user(
+            activity.assignee, activity.zaak, permission_name=permission.name
+        )
+        if atomic_permission:
+            atomic_permissions.append(atomic_permission)
+    return atomic_permissions
--- backend/src/zac/activities/api/serializers.py
+++ backend/src/zac/activities/api/serializers.py
@@ -1,8 +1,11 @@
+from django.db import transaction
+
 from rest_framework import serializers
 
 from zac.accounts.models import User
 
 from ..models import Activity, Event
+from .permission_loaders import add_permissions_for_activity_assignee
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -43,3 +46,24 @@ class ActivitySerializer(serializers.HyperlinkedModelSerializer):
                 "view_name": "activities:activity-detail",
             },
         }
+
+    @transaction.atomic
+    def create(self, validated_data):
+        activity = super().create(validated_data)
+
+        # add permissions to assignee
+        if activity.assignee:
+            add_permissions_for_activity_assignee(activity)
+        return activity
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        activity = super().update(instance, validated_data)
+
+        # add permissions to assignee
+        if (
+            validated_data.get("assignee")
+            and validated_data.get("assignee") != instance.assignee
+        ):
+            add_permissions_for_activity_assignee(activity)
+        return activity
