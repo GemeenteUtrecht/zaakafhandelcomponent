a1c2038f5d24761ade5bee40c47029a6bb1a65b6
Daniel Ammeraal <danielammeraal@gmail.com>
2021-04-23 17:37:19 +0200

    fix/camunda/select-documents

--- backend/src/zac/core/camunda/select_documents/context.py
+++ backend/src/zac/core/camunda/select_documents/context.py
@@ -1,8 +1,12 @@
-from zac.api.context import get_zaak_context
 from zac.camunda.data import Task
 from zac.camunda.process_instances import get_process_instance
 from zac.camunda.user_tasks import register
-from zac.core.services import fetch_zaaktype, get_informatieobjecttypen_for_zaaktype
+from zac.core.services import (
+    fetch_zaaktype,
+    get_documenten,
+    get_informatieobjecttypen_for_zaaktype,
+    get_zaak,
+)
 
 from .serializers import (
     DocumentSelectContext,
@@ -17,11 +21,16 @@ from .serializers import (
     DocumentSelectTaskSerializer,
 )
 def get_context(task: Task) -> DocumentSelectContext:
-    zaak_context = get_zaak_context(task, require_documents=True)
     process_instance = get_process_instance(task.process_instance_id)
+
+    zaak_url = process_instance.get_variable("hoofdZaakUrl")
+    zaak = get_zaak(zaak_url=zaak_url)
+    documenten, gone = get_documenten(zaak)
+
     related_zaaktype_url = process_instance.get_variable("zaaktype")
     related_zaaktype = fetch_zaaktype(related_zaaktype_url)
     informatieobjecttypen = get_informatieobjecttypen_for_zaaktype(related_zaaktype)
+
     return DocumentSelectContext(
-        documents=zaak_context.documents, informatieobjecttypen=informatieobjecttypen
+        documents=documenten, informatieobjecttypen=informatieobjecttypen
     )
