38d49bc2336d1f5e4e2956de78574f3d48555565
Sergei Maertens <sergei@maykinmedia.nl>
2021-05-18 15:26:59 +0200

    :bookmark: Bump minor version
    This creates a stable release planned to go to ACC environment.

--- backend/doc/conf.py
+++ backend/doc/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Maykin Media, Gemeente Utrecht"
 author = "Maykin Media, Gemeente Utrecht"
 
 # The full version, including alpha/beta/rc tags
-release = "0.34.0"
+release = "0.35.0"
 
 
 # -- General configuration ---------------------------------------------------
