e6e91a776e5215eccbbc7b0f8688fd9be6bd9c40
Daniel Ammeraal <danielammeraal@gmail.com>
2021-05-19 11:56:13 +0200

    fix: zrc_client.destroy -> zrc_client.delete

--- backend/src/zac/core/services.py
+++ backend/src/zac/core/services.py
@@ -676,14 +676,13 @@ def update_rol(rol_url: str, new_rol: Dict) -> Rol:
     zrc_client = _client_from_url(new_rol["zaak"])
 
     # Destroy old rol
-    zrc_client.destroy("rol", url=rol_url)
+    zrc_client.delete("rol", url=rol_url)
 
     # Invalidate the cache of the rol that is destroyed
     cache.delete(f"rol:{rol_url}")
 
     # Create new rol
     rol = zrc_client.create("rol", new_rol)
-
     return factory(Rol, rol)
 
 
@@ -785,7 +784,6 @@ def zet_status(zaak: Zaak, statustype: StatusType, toelichting: str = "") -> Sta
     return status
 
 
-@cache_result("get_behandelaar_zaken:{user.username}:{ordering}", timeout=AN_HOUR)
 def get_behandelaar_zaken(user: User, ordering: List = []) -> List[Zaak]:
     """
     Retrieve zaken where `user` is a medewerker in the role of behandelaar.
