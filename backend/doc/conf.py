43eea0699363a7447523a6106519a38a95940126
Sergei Maertens <sergei@maykinmedia.nl>
2021-05-25 14:25:49 +0200

    :bookmark: Bump to 0.35.2

--- backend/doc/conf.py
+++ backend/doc/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Maykin Media, Gemeente Utrecht"
 author = "Maykin Media, Gemeente Utrecht"
 
 # The full version, including alpha/beta/rc tags
-release = "0.35.1"
+release = "0.35.2"
 
 
 # -- General configuration ---------------------------------------------------
