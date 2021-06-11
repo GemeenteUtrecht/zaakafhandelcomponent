4a2623b9e95291a4f0d38d2877682c81c2b60725
silvia <silvia@maykinmedia.nl>
2021-06-11 17:32:18 +0200

    :construction: [#338] Retrieve related objects from Object API

--- backend/src/zac/core/zaakobjecten.py
+++ backend/src/zac/core/zaakobjecten.py
@@ -5,6 +5,8 @@ import requests
 from zgw_consumers.api_models.zaken import ZaakObject
 
 from zac.contrib.kadaster.bag import fetch_pand, fetch_verblijfsobject
+from zac.core.models import CoreConfig
+from zac.core.utils import fetch_objects
 
 
 def noop(url: str) -> str:
@@ -27,6 +29,22 @@ class ZaakObjectGroup:
         self.items = [
             self.retriever(item.object) if item.object else item for item in items
         ]
+
+        # Resolve if some of the related objects are in the objects API
+        config = CoreConfig.get_solo()
+        object_api = config.primary_objects_api
+
+        objects_in_object_api = []
+        other_objects = []
+
+        for item in self.items:
+            if isinstance(item, str) and item.startswith(object_api.api_root):
+                objects_in_object_api.append(item)
+            else:
+                other_objects.append(other_objects)
+
+        objects_in_object_api = fetch_objects(objects_in_object_api)
+        self.items = objects_in_object_api + other_objects
 
 
 GROUPS = {
