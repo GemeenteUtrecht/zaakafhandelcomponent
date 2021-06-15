ef82d092e06dd85e2b868adc5e19531d03d88765
silvia <silvia@maykinmedia.nl>
2021-06-15 13:59:01 +0200

    :lipstick: [#344] Added related object component

--- frontend/zac-ui/libs/features/zaak-detail/src/lib/gerelateerde-objecten/gerelateerde-objecten.component.ts
+++ frontend/zac-ui/libs/features/zaak-detail/src/lib/gerelateerde-objecten/gerelateerde-objecten.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {GerelateerdeObjectenService } from "./gerelateerde-objecten.service";
 
 @Component({
   selector: 'gu-gerelateerde-objecten',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gerelateerde-objecten.component.scss']
 })
 export class GerelateerdeObjectenComponent implements OnInit {
+  @Input() bronorganisatie: string;
+  @Input() identificatie: string;
 
-  constructor() { }
+  /** @type {boolean} Whether this component is loading. */
+  isLoading: boolean;
+
+  /** @type {Object[]} The list of groups of objects (Related objects are grouped on objecttype) */
+  relatedObjects: Array<Object>;
+
+  constructor(private gerelateerdeObjectenService: GerelateerdeObjectenService) { }
 
   ngOnInit(): void {
+    this.fetchRelatedObjects();
+  }
+
+  /**
+   * Fetches the objects related to a zaak
+   */
+  fetchRelatedObjects() {
+    this.isLoading = true;
+
+    this.gerelateerdeObjectenService.getRelatedObjects(
+      this.bronorganisatie,
+      this.identificatie
+    ).subscribe(
+      (data) => {
+        this.relatedObjects = data;
+        this.isLoading = false;
+      },
+      (error) => {
+        console.error(error);
+        this.isLoading = false;
+      }
+    );
   }
 
 }
