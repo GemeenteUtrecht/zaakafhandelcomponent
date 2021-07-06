e35e1f76611fb647a1044975e84b14e9490bb846
daniel <danielammeraal@gmail.com>
2021-07-06 14:56:28 +0200

    Added tests

--- backend/src/zac/accounts/api/filters.py
+++ backend/src/zac/accounts/api/filters.py
@@ -2,12 +2,16 @@ from django_filters import rest_framework as filters
 from django_filters.widgets import QueryArrayWidget
 
 
-class UsernameInFilter(filters.BaseInFilter, filters.CharFilter):
+class StringInFilter(filters.BaseInFilter, filters.CharFilter):
     pass
 
 
 class UserFilter(filters.FilterSet):
-    include = UsernameInFilter(field_name="username", widget=QueryArrayWidget)
-    exclude = UsernameInFilter(
+    include_username = StringInFilter(field_name="username", widget=QueryArrayWidget)
+    exclude_username = StringInFilter(
         field_name="username", widget=QueryArrayWidget, exclude=True
+    )
+    include_email = StringInFilter(field_name="email", widget=QueryArrayWidget)
+    exclude_email = StringInFilter(
+        field_name="email", widget=QueryArrayWidget, exclude=True
     )
