58c8f526a55183cd9836dd77bf2cf33fff264d29
daniel <danielammeraal@gmail.com>
2021-07-08 10:50:26 +0200

    black

--- backend/src/zac/accounts/migrations/0046_merge_20210708_0809.py
+++ backend/src/zac/accounts/migrations/0046_merge_20210708_0809.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0045_auto_20210702_1240'),
-        ('accounts', '0038_auto_20210630_1251'),
+        ("accounts", "0045_auto_20210702_1240"),
+        ("accounts", "0038_auto_20210630_1251"),
     ]
 
-    operations = [
-    ]
+    operations = []
