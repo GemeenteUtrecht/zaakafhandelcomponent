d161e402a9287cd0ffc835be65e424f31c62c092
silvia <silvia@maykinmedia.nl>
2021-08-04 17:14:24 +0200

    :sparkles: Added UUID to User

--- backend/src/zac/accounts/models.py
+++ backend/src/zac/accounts/models.py
@@ -34,6 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     Use the built-in user model.
     """
 
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     username = models.CharField(
         _("username"),
         max_length=150,
