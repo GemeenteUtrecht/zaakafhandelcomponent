7a91baf560966bc20f388c0d1a979dc0db0963b4
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-08-18 18:02:15 +0200

    :white_check_mark: test role endpoints

---
+++ backend/src/zac/accounts/api/tests/test_roles.py
@@ -0,0 +1,139 @@
+from django.urls import reverse, reverse_lazy
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from zac.core.permissions import zaken_inzien, zaken_request_access
+
+from ...models import Role
+from ...tests.factories import (
+    RoleFactory,
+    StaffUserFactory,
+    SuperUserFactory,
+    UserFactory,
+)
+
+
+class RolePermissionsTests(APITestCase):
+    url = reverse_lazy("role-list")
+
+    def test_no_staff_user(self):
+        user = UserFactory.create()
+        self.client.force_authenticate(user)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_staff_user(self):
+        user = StaffUserFactory.create()
+        self.client.force_authenticate(user)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_superuser(self):
+        user = SuperUserFactory.create()
+        self.client.force_authenticate(user)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class RoleAPITests(APITestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.user = SuperUserFactory.create()
+        self.client.force_authenticate(self.user)
+
+    def test_list_roles(self):
+        role1 = RoleFactory.create(permissions=[zaken_inzien.name])
+        role2 = RoleFactory.create(permissions=[zaken_request_access.name])
+        url = reverse("role-list")
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.json(),
+            [
+                {
+                    "id": role1.id,
+                    "name": role1.name,
+                    "permissions": [zaken_inzien.name],
+                },
+                {
+                    "id": role2.id,
+                    "name": role2.name,
+                    "permissions": [zaken_request_access.name],
+                },
+            ],
+        )
+
+    def test_retrieve_auth_profile(self):
+        role = RoleFactory.create(permissions=[zaken_inzien.name])
+        url = reverse("role-detail", args=[role.id])
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.json(),
+            {"id": role.id, "name": role.name, "permissions": [zaken_inzien.name]},
+        )
+
+    def test_create_auth_profile(self):
+        url = reverse("role-list")
+        data = {"name": "some name", "permissions": [zaken_inzien.name]}
+
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Role.objects.count(), 1)
+
+        role = Role.objects.get()
+
+        self.assertEqual(role.name, "some name")
+        self.assertEqual(role.permissions, ["zaken:inzien"])
+
+    def test_create_role_incorrect_permission_name(self):
+        url = reverse("role-list")
+        data = {"name": "some name", "permissions": ["some permission"]}
+
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.json(),
+            {"permissions": [['"some permission" is een ongeldige keuze.']]},
+        )
+
+    def test_update_role(self):
+        role = RoleFactory.create(name="old name", permissions=[zaken_inzien.name])
+        url = reverse("role-detail", args=[role.id])
+        data = {"name": "new name", "permissions": [zaken_request_access.name]}
+
+        response = self.client.put(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        role.refresh_from_db()
+
+        self.assertEqual(role.name, "new name")
+        self.assertEqual(role.permissions, [zaken_request_access.name])
+
+    def test_update_role_incorrect_permission_name(self):
+        role = RoleFactory.create(name="old name", permissions=[zaken_inzien.name])
+        url = reverse("role-detail", args=[role.id])
+        data = {"name": "new name", "permissions": ["some permission"]}
+
+        response = self.client.put(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.json(),
+            {"permissions": [['"some permission" is een ongeldige keuze.']]},
+        )
