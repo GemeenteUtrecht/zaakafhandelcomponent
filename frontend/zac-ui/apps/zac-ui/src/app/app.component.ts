bfdcd1d98bb5adf2dc0aa50a30d37d42b2c8664e
Sven van de Scheur <svenvandescheur@gmail.com>
2021-08-20 15:43:09 +0200

    :art: - Clean up AppComponent.

--- frontend/zac-ui/apps/zac-ui/src/app/app.component.ts
+++ frontend/zac-ui/apps/zac-ui/src/app/app.component.ts
@@ -1,36 +1,66 @@
 import {Component, OnInit} from '@angular/core';
-import {menuItems, bottomMenuItems, MenuItem} from './constants/menu';
 import {NavigationEnd, Router} from '@angular/router';
 import {filter} from 'rxjs/operators';
-import {ApplicationHttpClient, UserService, ZaakService} from '@gu/services';
-import {SnackbarService} from "@gu/components";
+import {SnackbarService} from '@gu/components';
+import {UserService, ZaakService} from '@gu/services';
+import {menuItems, bottomMenuItems, MenuItem} from './constants/menu';
 
+
+/**
+ * <gu-zac-ui-root></gu-zac-ui-root>
+ *
+ * Application root.
+ */
 @Component({
   selector: 'gu-zac-ui-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  /** @type {string} Possible error message to show. */
   readonly errorMessage: 'Er is een fout opgetreden bij het laden van de applicatie.'
 
-  title = 'zac-ui';
+  /** @type {string} The (relative) url to the logo. */
   logoUrl = 'assets/gemeente-utrecht-logo.svg';
+
+  /** @type {string} The (relative) url to the mobile logo. */
   mobileLogoUrl = 'assets/schild.png';
+
+  /** @type {string} The string representation of the current user. */
   currentUser: string;
 
+  /** @type {MenuItem[]} The menu items to show in the center of the menu. */
   menuItems: MenuItem[] = menuItems;
+
+  /** @type {MenuItem[]} The menu items to show in the bottom of the menu. */
   bottomMenuItems: MenuItem[] = bottomMenuItems;
+
+  /** @type {string} Selected menu item. */
   selectedMenu: string;
 
+  /**
+   * Constructor method.
+   * @param {Router} router
+   * @param {SnackbarService} snackbarService
+   * @param {UserService} userService
+   * @param {ZaakService} zaakService
+   */
   constructor(
-    private http: ApplicationHttpClient,
     private router: Router,
     private snackbarService: SnackbarService,
     private userService: UserService,
     private zaakService: ZaakService,) {
   }
 
-  ngOnInit() {
+  //
+  // Angular lifecycle.
+  //
+
+  /**
+   * A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive. Define an
+   * ngOnInit() method to handle any additional initialization tasks.
+   */
+  ngOnInit(): void {
     this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => {
@@ -39,6 +69,17 @@ export class AppComponent implements OnInit {
         window.scrollTo(0, 0);
       });
 
+    this.getContextData();
+  }
+
+  //
+  // Context.
+  //
+
+  /**
+   * Retrieves the current user.
+   */
+  getContextData(): void {
     this.userService.getCurrentUser()
       .subscribe(([user, isHijacked]) => {
         this.currentUser = this.userService.stringifyUser(user);
@@ -46,23 +87,32 @@ export class AppComponent implements OnInit {
         if (isHijacked) {
           this.snackbarService.openSnackBar(`Je werkt nu namens ${this.currentUser}`, 'Stoppen', 'warn', 0)
             .afterDismissed()
-            .subscribe(this.releaseHijack.bind(this));
+            .subscribe(this.hijackSnackBarDismissed.bind(this));
         }
       });
-  }
-
-  navigateToZaak(zaak: { bronorganisatie: string, identificatie: string }) {
-    this.zaakService.navigateToCase(zaak);
   }
 
   //
   // Events
   //
 
-  releaseHijack() {
+  /**
+   * Gets called when the hijack snackbar is dismissed.
+   * Releases hijack.
+   */
+  hijackSnackBarDismissed(): void {
     this.userService.releaseHijack().subscribe(
       () => this.router.navigate([''])
     );
+  }
+
+  /**
+   * Gets called when a Zaak (case) is selected.
+   * Navigates to the case's detail view.
+   * @param {Zaak} zaak
+   */
+  zaakSelected(zaak: { bronorganisatie: string, identificatie: string }): void {
+    this.zaakService.navigateToCase(zaak);
   }
 
   //
