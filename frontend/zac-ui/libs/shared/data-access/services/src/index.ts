753370e834c33eb4e276d96eb2b1d418defaa7b5
Sven van de Scheur <svenvandescheur@gmail.com>
2021-08-23 17:37:02 +0200

    :sparkles: #819 - Move zaak search from zaak objecten search to search form.

--- frontend/zac-ui/libs/shared/data-access/services/src/index.ts
+++ frontend/zac-ui/libs/shared/data-access/services/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/http/http-client';
 export * from './lib/shared-data-access-services.module';
 export * from './lib/user/user.service'
 export * from './lib/zaak/zaak.service'
+export * from './lib/zaak/zaak-object.service'
