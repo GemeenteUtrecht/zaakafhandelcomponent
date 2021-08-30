20af8b95959495ea981bae2a7ef1e0188562f167
Kelvin Chan <kelvinchancy95@gmail.com>
2021-08-30 14:19:53 +0200

    Added documentation for paginator

--- frontend/zac-ui/libs/shared/ui/components/src/lib/components/paginator/paginator.component.ts
+++ frontend/zac-ui/libs/shared/ui/components/src/lib/components/paginator/paginator.component.ts
@@ -1,13 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 
+/**
+ *     <gu-paginator (page)="onPageSelect($event)" [length]="resultLength"></gu-paginator>
+ *
+ *     Generic paginator componentm based on mat-paginator
+ *
+ *     Requires length: Total amount of results
+ *     Takes pageSize: Number of results per page
+ *
+ *     Emits page: details about the paginator state
+ */
 @Component({
   selector: 'gu-paginator',
   templateUrl: './paginator.component.html'
 })
 export class PaginatorComponent {
 
-  // MatPaginator Inputs
   @Input() length = 0;
   @Input() pageSize = 100;
 
