ea289707c670d9ce5c1cd923db4c43f8b5100f72
Sven van de Scheur <svenvandescheur@gmail.com>
2021-09-10 12:37:47 +0200

    :construction:

--- frontend/zac-ui/libs/features/contezza-document-search/src/lib/features-contezza-document-search.component.ts
+++ frontend/zac-ui/libs/features/contezza-document-search/src/lib/features-contezza-document-search.component.ts
@@ -1,9 +1,31 @@
-import {Component} from "@angular/core";
+import {DOCUMENT} from '@angular/common';
+import {Component, Inject, OnInit, Renderer2} from '@angular/core';
 
 @Component({
-  selector: 'gu-features-contezza-document-search',
-  templateUrl: './features-contezza-document-search.component.html',
-  styleUrls: ['./features-contezza-document-search.component.scss'],
+    selector: 'gu-features-contezza-document-search',
+    templateUrl: './features-contezza-document-search.component.html',
+    styleUrls: ['./features-contezza-document-search.component.scss'],
 })
-export class FeaturesContezzaDocumentSearchComponent {
+export class FeaturesContezzaDocumentSearchComponent implements OnInit {
+    /**
+     * Constructor method.
+     * @param {Renderer2} renderer2
+     * @param {Document} document
+     */
+    constructor(private renderer2: Renderer2, @Inject(DOCUMENT) private document: Document) {
+    }
+
+    //
+    // Angular lifecycle.
+    //
+
+    /**
+     * A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive. Define an
+     * ngOnInit() method to handle any additional initialization tasks.
+     */
+    ngOnInit() {
+        const script = this.renderer2.createElement('script');
+        script.src = '/ui/assets/contezza-documentlist.js';
+        this.renderer2.appendChild(this.document.body, script);
+    }
 }
