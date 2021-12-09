dfc9dc7b316577e14892d412e9cdf774383a7dcb
damm89 <danielammeraal@gmail.com>
2021-12-09 14:15:38 +0100

    :bug: Call .is_valid() on serializer in fetch_process_instances

--- backend/src/zac/camunda/api/views.py
+++ backend/src/zac/camunda/api/views.py
@@ -66,6 +66,7 @@ class ProcessInstanceFetchView(APIView):
         zaak_url = request.GET.get("zaak_url")
         if not zaak_url:
             err_serializer = ErrorSerializer(data={"detail": "missing zaak_url"})
+            err_serializer.is_valid()
             return Response(err_serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
         process_instances = get_process_instances(zaak_url)
