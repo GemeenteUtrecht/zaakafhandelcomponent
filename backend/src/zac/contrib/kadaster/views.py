1ae4f527fa3c874cd001e151ce41a1383a4f6cd9
damm89 <danielammeraal@gmail.com>
2022-01-05 14:23:38 +0100

    :memo: Add docstrings to kadaster api views

--- backend/src/zac/contrib/kadaster/views.py
+++ backend/src/zac/contrib/kadaster/views.py
@@ -34,6 +34,12 @@ class AdresSearchView(APIView):
         ],
     )
     def get(self, request: Request, *args, **kwargs):
+        """
+        This endpoint allows users to search for address(es) (suggestions) using the PDOK locatieserver. The `id` of the address(es) can then be used to fetch a pand or a verblijfsobject from the BAG API.
+
+        Please see https://www.pdok.nl/restful-api/-/article/pdok-locatieserver-1 for more details on how to use the PDOK locatieserver API and how to query for address suggestions.
+        Be cognizant of the fact that we only allow use of the `q` query parameter. The API request the ZAC makes to the locatieserver will have the  `fq=bron:bag AND type:adres` query parameter and its value added to every request.
+        """
         query = request.GET.get("q")
         if not query:
             raise serializers.ValidationError(_("Missing query parameter 'q'"))
@@ -55,13 +61,19 @@ class PandFetchView(APIView):
                 OpenApiTypes.STR,
                 OpenApiParameter.QUERY,
                 description=_(
-                    "The ID of the BAG object. The ID can be found using the BAG address suggestions."
+                    "The `id` of the BAG object. The `id` can be found using the BAG address suggestions."
                 ),
                 required=True,
             )
         ],
     )
     def get(self, request: Request, *args, **kwargs):
+        """
+        This endpoint allows users to fetch a pand and relevant metadata from the BAG API.
+
+        Once users have retrieved the `id` of an address from the PDOK locatieserver, they can use the `id` to fetch a pand from the BAG API.
+        Please see https://github.com/lvbag/BAG-API for further documentation on the BAG API.
+        """
         address_id = request.GET.get("id")
         if not address_id:
             raise serializers.ValidationError(_("Missing query parameter 'id'"))
@@ -83,13 +95,19 @@ class VerblijfsobjectFetchView(APIView):
                 OpenApiTypes.STR,
                 OpenApiParameter.QUERY,
                 description=_(
-                    "The ID of the BAG object. The ID can be found using the BAG address suggestions."
+                    "The `id` of the BAG object. The `id` can be found using the BAG address suggestions."
                 ),
                 required=True,
             )
         ],
     )
     def get(self, request: Request, *args, **kwargs):
+        """
+        This endpoint allows users to fetch a verblijfsobject and relevant metadata from the BAG API.
+
+        Once users have retrieved the `id` of an address from the PDOK locatieserver, they can use the `id` to fetch a verblijfsobject from the BAG API.
+        Please see https://github.com/lvbag/BAG-API for further documentation on the BAG API.
+        """
         address_id = request.GET.get("id")
         if not address_id:
             raise serializers.ValidationError(_("Missing query parameter 'id'"))
