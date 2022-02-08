554e7ffaf62ccdbefd1f9f773604cc3625bdbec0
Sven van de Scheur <svenvandescheur@gmail.com>
2022-02-08 12:41:55 +0100

    :sparkles: - Allow BAG object to be used as a zaak (case) geometry.

--- frontend/zac-ui/libs/shared/ui/components/src/lib/components/map/map.ts
+++ frontend/zac-ui/libs/shared/ui/components/src/lib/components/map/map.ts
@@ -2,14 +2,17 @@ import {Geometry, Position} from '@gu/models';
 
 export interface MapGeometry {
   geometry: Geometry,
+  actions?: MapAction[],
   editable?: boolean
   title?: string,
   onClick?: Function,
   onChange?: Function
+
 }
 
 export interface MapMarker {
   coordinates: Position,
+  actions?: MapAction[],
   editable?: boolean,
   iconAnchor?: number[],
   iconUrl?: string,
@@ -20,4 +23,9 @@ export interface MapMarker {
   title?: string,
   onClick?: Function,
   onChange?: Function
+}
+
+export interface MapAction {
+  label: string,
+  onClick: Function,
 }
