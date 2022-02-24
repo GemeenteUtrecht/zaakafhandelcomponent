81cca35ef652fd42d623bd216f218001adc2d512
Kelvin Chan <kelvinchancy95@gmail.com>
2022-02-24 13:34:53 +0100

    :bookmark: Bump to 0.43.0

--- backend/doc/conf.py
+++ backend/doc/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Maykin Media, Gemeente Utrecht"
 author = "Maykin Media, Gemeente Utrecht"
 
 # The full version, including alpha/beta/rc tags
-release = "0.42.7"
+release = "0.43.0"
 
 
 # -- General configuration ---------------------------------------------------
