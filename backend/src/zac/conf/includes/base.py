a36e4b83f2ed7ac4efbb95005493645e43a9c5cb
damm89 <danielammeraal@gmail.com>
2022-03-16 20:47:44 +0100

    :sparkles: Add title to component schema

--- backend/src/zac/conf/includes/base.py
+++ backend/src/zac/conf/includes/base.py
@@ -443,6 +443,7 @@ SPECTACULAR_SETTINGS = {
         "drf_spectacular.hooks.postprocess_schema_enums",
         "drf_spectacular.contrib.djangorestframework_camel_case.camelize_serializer_fields",
         "zac.api.drf_spectacular.djangorestframework_camel_case.camelize_discriminators",
+        "zac.api.drf_spectacular.component_titles.add_title_to_component_schema",
     ],
     "TOS": None,
     # Optional: MAY contain "name", "url", "email"
