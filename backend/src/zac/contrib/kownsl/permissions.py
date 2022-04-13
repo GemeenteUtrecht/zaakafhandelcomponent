3847cff454a027f622f178a2b5392c5bd7b5f517
damm89 <danielammeraal@gmail.com>
2022-04-13 16:48:09 +0200

    :passport_control: Update permissions in zac.contrib.kownsl to allow for force editing after ZAAK closes

--- backend/src/zac/contrib/kownsl/permissions.py
+++ backend/src/zac/contrib/kownsl/permissions.py
@@ -1,13 +1,11 @@
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework import permissions
-from rest_framework.request import Request
-from rest_framework.views import APIView
 from zgw_consumers.api_models.base import factory
 
 from zac.api.permissions import DefinitionBasePermission
 from zac.camunda.constants import AssigneeTypeChoices
-from zac.core.api.permissions import CanReadZaken
+from zac.core.api.permissions import BaseConditionalPermission, CanReadZaken
 from zac.core.camunda.utils import resolve_assignee
 from zac.core.services import get_zaak
 
@@ -27,20 +25,12 @@ class CanLockReview(permissions.BasePermission):
         return True
 
 
-class CanReadOrLockReviews:
+class CanReadOrLockReviews(BaseConditionalPermission):
     def get_permission(self, request) -> DefinitionBasePermission:
         if request.method in permissions.SAFE_METHODS:
             return CanReadZaken()
         else:
             return CanLockReview()
-
-    def has_permission(self, request: Request, view: APIView) -> bool:
-        permission = self.get_permission(request)
-        return permission.has_permission(request, view)
-
-    def has_object_permission(self, request: Request, view: APIView, obj) -> bool:
-        permission = self.get_permission(request)
-        return permission.has_object_permission(request, view, obj)
 
 
 class ReviewIsUnlocked(permissions.BasePermission):
