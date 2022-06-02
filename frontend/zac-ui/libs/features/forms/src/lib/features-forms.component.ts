af340fa942c29a8837efb3e1df2b7f4035707c14
Sven van de Scheur <svenvandescheur@gmail.com>
2022-06-02 10:27:18 +0200

    :bug: #1490 - ZAC: links naar OpenForms zijn niet aanklikbaar als je in de ZAC naar 'formulier' gaat. (#666)

--- frontend/zac-ui/libs/features/forms/src/lib/features-forms.component.ts
+++ frontend/zac-ui/libs/features/forms/src/lib/features-forms.component.ts
@@ -10,18 +10,45 @@ import {Form} from "./features-forms.model";
   styleUrls: ['./features-forms.component.scss'],
 })
 export class FeaturesFormsComponent implements OnInit {
+  /** @type {Form[]} The forms. */
   forms: Form[] = [];
 
+  /** @type {Table} The table data. */
+  tableData: Table = null;
+
+  /**
+   * Constructor method.
+   * @param featuresFormsService
+   */
   constructor(private featuresFormsService: FeaturesFormsService) {
   }
+
+  //
+  // Angular lifecycle.
+  //
 
   /**
    * A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
    * Define an `ngOnInit()` method to handle any additional initialization tasks.
    */
   ngOnInit() {
+    this.getContextData();
+  }
+
+  //
+  // Context.
+  //
+
+  /**
+   * Fetches the forms and gets the table data.
+   */
+  getContextData() {
     this.featuresFormsService.getForms().subscribe(
-      (data) => this.forms = data,
+      (data) => {
+        this.forms = data;
+        this.tableData = this.getTableData();
+
+      },
       (error) => console.error(error),
     );
   }
@@ -30,10 +57,10 @@ export class FeaturesFormsComponent implements OnInit {
    * Return the table data for the forms table.
    * @return {Table}
    */
-  get tableData(): Table {
+  getTableData(): Table {
     return {
       headData: ['Naam', 'URL'],
-      bodyData: this.bodyData,
+      bodyData: this.getBodyData(),
     }
   }
 
@@ -41,7 +68,7 @@ export class FeaturesFormsComponent implements OnInit {
    * Returns the RowData[] for use in the table's bodyData.
    * @return RowData[]
    */
-  get bodyData(): RowData[] {
+  getBodyData(): RowData[] {
     return this.forms.map(form => ({
         cellData: {
           'Naam': form.name,
@@ -54,5 +81,29 @@ export class FeaturesFormsComponent implements OnInit {
         }
       }
     ));
+  }
+
+  //
+  // Events.
+  //
+
+  onSort(sortData) {
+    const key = (sortData.value.toUpperCase() === 'URL') ? 'slug' : sortData.value;
+    const order = sortData.order.toUpperCase();
+
+    this.forms = this.forms.sort((a, b) => {
+      const valueA = String(a[key]).toUpperCase();
+      const valueB = String(b[key]).toUpperCase();
+
+      if (valueA < valueB) {
+        return (order === 'ASC') ? -1 : 1;
+      }
+      if (valueA > valueB) {
+        return (order === 'ASC') ? 1 : -1;
+      }
+      return 0;
+    })
+
+    this.tableData = this.getTableData();
   }
 }
