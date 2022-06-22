41a8f4f03d1788df4afbf2d2d50023ec6a9a0562
Jordi Teterissa <jordi@teterissa.nl>
2022-06-22 15:01:22 +0200

    substitute vars in app.config.json for Alfresco

--- frontend/zac-ui/bin/docker_start.sh
+++ frontend/zac-ui/bin/docker_start.sh
@@ -2,4 +2,5 @@
 #!/bin/sh
 
 envsubst < /apps/zac-ui/assets/index.html > /apps/zac-ui/assets/index.html.tmp && mv /apps/zac-ui/assets/index.html.tmp /apps/zac-ui/assets/index.html
+envsubst < /apps/zac-ui/app.config.json > /apps/zac-ui/app.config.json.tmp && mv /apps/zac-ui/app.config.json.tmp /apps/zac-ui/app.config.json
 nginx -g "daemon off;"
