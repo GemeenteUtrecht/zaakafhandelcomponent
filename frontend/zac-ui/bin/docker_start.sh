40504c142027232d6464d6b24ae52cddf1c36f41
Kelvin Chan <kelvinchancy95@gmail.com>
2022-06-22 15:30:58 +0200

    Fix index.html path

--- frontend/zac-ui/bin/docker_start.sh
+++ frontend/zac-ui/bin/docker_start.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
 
-envsubst < /apps/zac-ui/assets/index.html > /apps/zac-ui/assets/index.html.tmp && mv /apps/zac-ui/assets/index.html.tmp /apps/zac-ui/assets/index.html
+envsubst < /apps/zac-ui/index.html > /apps/zac-ui/index.html.tmp && mv /apps/zac-ui/index.html.tmp /apps/zac-ui/index.html
 envsubst < /apps/zac-ui/app.config.json > /apps/zac-ui/app.config.json.tmp && mv /apps/zac-ui/app.config.json.tmp /apps/zac-ui/app.config.json
 nginx -g "daemon off;"
