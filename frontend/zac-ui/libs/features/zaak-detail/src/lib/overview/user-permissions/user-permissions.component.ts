b8807d7f3752bf630c533a7492d03172bc0bc6c9
Sven van de Scheur <svenvandescheur@gmail.com>
2022-07-05 14:25:21 +0200

    :sparkles: #1559 - Als medewerker wil ik graag een waarschuwing als ik iemands rechten verwijder. (#695)

--- frontend/zac-ui/libs/features/zaak-detail/src/lib/overview/user-permissions/user-permissions.component.ts
+++ frontend/zac-ui/libs/features/zaak-detail/src/lib/overview/user-permissions/user-permissions.component.ts
@@ -1,8 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { ZaakPermission, UserPermission, Table, Zaak } from '@gu/models';
+import {ZaakPermission, UserPermission, Table, Zaak} from '@gu/models';
 import {ZaakService} from "@gu/services";
 import {PermissionsService} from './permissions.service';
-import { ModalService, TableButtonClickEvent } from '@gu/components';
+import {ModalService} from '@gu/components';
 
 /**
  * <gu-user-permissions [bronorganisatie]="bronorganisatie" [identificatie]="identificatie"></gu-user-permissions>
@@ -13,172 +13,189 @@ import { ModalService, TableButtonClickEvent } from '@gu/components';
  * Requires identificatie: string input to identify the case (zaak).
  */
 @Component({
-    providers: [PermissionsService],
-    selector: 'gu-user-permissions',
-    styleUrls: ['./user-permissions.component.scss'],
-    templateUrl: './user-permissions.component.html',
+  providers: [PermissionsService],
+  selector: 'gu-user-permissions',
+  styleUrls: ['./user-permissions.component.scss'],
+  templateUrl: './user-permissions.component.html',
 })
 export class UserPermissionsComponent implements OnInit {
-    @Input() zaak: Zaak;
+  @Input() zaak: Zaak;
 
-    /** @type {string} Error message. */
-    errorDetailMessage = '';
+  /** @type {string} Error message. */
+  errorDetailMessage = '';
 
-    /** @type {boolean} Whether this component is loading. */
-    isLoading = false;
+  /** @type {boolean} Whether this component is loading. */
+  isLoading = false;
 
-    /** @type {Table} The Table to render. */
-    table: Table = null;
+  /** @type {ZaakPermission} The selected (for removal) permission. */
+  selectedPermission: ZaakPermission = null;
 
-    /** @type {Table} The short version of Table to render. */
-    shortTable: Table = null;
+  /** @type {Table} The Table to render. */
+  table: Table = null;
 
-    /** @type {UserPermission[]} The user permissions. */
-    userPermissions: UserPermission[] = [];
+  /** @type {Table} The short version of Table to render. */
+  shortTable: Table = null;
 
-    /** @type {boolean} Wether table rows are all shown */
-    isExpanded = false;
+  /** @type {UserPermission[]} The user permissions. */
+  userPermissions: UserPermission[] = [];
 
-    constructor(
-      private permissionsService: PermissionsService,
-      private modalService: ModalService,
-      private zaakService: ZaakService,
-    ) {
-    }
+  /** @type {boolean} Wether table rows are all shown */
+  isExpanded = false;
 
-    /**
-     * Updates the component using a public interface.
-     */
-    public update() {
-      this.getContextData();
-    }
+  constructor(
+    private permissionsService: PermissionsService,
+    private modalService: ModalService,
+    private zaakService: ZaakService,
+  ) {
+  }
 
-    //
-    // Getters / setters.
-    //
-
-    /**
-     * Whether user can force edit a closed case.
-     * @returns {boolean}
-     */
-    get canForceEdit(): boolean {
-      return !this.zaak.resultaat || this.zaak.kanGeforceerdBijwerken;
-    }
-
-    //
-    // Angular lifecycle.
-    //
-
-    /**
-     * A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive. Define an
-     * ngOnInit() method to handle any additional initialization tasks.
-     */
-    ngOnInit(): void {
-        this.getContextData()
-    }
-
-    //
-    // Context.
-    //
-
-    /**
-     * Fetches the user permissions.
-     */
-    getContextData(): void {
-        this.zaakService.listCaseUsers(this.zaak.bronorganisatie, this.zaak.identificatie).subscribe(
-            (userPermissions: UserPermission[]): void => {
-                this.table = this.userPermissionsAsTable(userPermissions);
-
-                this.shortTable = {...this.table};
-                this.shortTable.bodyData = this.shortTable.bodyData.slice(0, 3);
-
-                this.isLoading = false;
-            },
-            this.reportError.bind(this),
-        );
-    }
-
-
-    /**
-     * Returns user permissions as table.
-     * @param {UserPermission[]} userPermissions
-     * @return {Table}
-     */
-    userPermissionsAsTable(userPermissions: UserPermission[]): Table {
-        const bodyData = userPermissions.reduce((acc, userPermission) => {
-            const userRows = userPermission.permissions.map((permission: ZaakPermission) => ({
-                cellData: {
-                    user: {
-                        type: 'text',
-                        label: userPermission.username,
-                    },
-                    permission: {
-                        type: 'chip',
-                        label: permission.permission
-                    },
-                    // Hide button if case is closed and the user is not allowed to force edit
-                    delete: !this.zaak.resultaat || this.zaak.kanGeforceerdBijwerken ? {
-                        type: 'button',
-                        label: 'Verwijderen',
-                        value: permission,
-                    } : ''
-                },
-                nestedTableData: new Table(['Reden', 'Commentaar'], [{
-                    cellData: {
-                        reason: permission.reason,
-                        comment: permission["comment"]
-                    }
-                },
-                ]),
-                expandData: ''
-            }));
-            return [...acc, ...userRows];
-        }, []);
-
-        return new Table(
-            [
-                'Gebruiker',
-                'Rechten',
-                'Acties',
-            ], bodyData);
-    }
-
-    //
-    // Events.
-    //
-
-    /**
-     * Opens modal.
-     * @param {string} id
-     */
-    openModal(id: string) {
-        this.modalService.open(id);
-    }
-
-    /**
-     * Expand table rows.
-     */
-    expand() {
-        this.isExpanded = !this.isExpanded;
-      }
-
-    /**
-     * Gets called when delete button is clicked, remover user permission.
-     * @param {TableButtonClickEvent} event
-     */
-    buttonClick(event: TableButtonClickEvent): void {
-        const permission = event.delete;
-        this.permissionsService.deletePermission(permission).subscribe(
-            (): void => {
-                this.getContextData();
-            },
-            this.reportError.bind(this),
-        );
-    }
+  /**
+   * Updates the component using a public interface.
+   */
+  public update() {
+    this.getContextData();
+  }
 
   //
-  // Error handling.
+  // Getters / setters.
   //
+
+  /**
+   * Whether user can force edit a closed case.
+   * @returns {boolean}
+   */
+  get canForceEdit(): boolean {
+    return !this.zaak.resultaat || this.zaak.kanGeforceerdBijwerken;
+  }
+
+  //
+  // Angular lifecycle.
+  //
+
+  /**
+   * A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive. Define an
+   * ngOnInit() method to handle any additional initialization tasks.
+   */
+  ngOnInit(): void {
+    this.getContextData()
+  }
+
+  //
+  // Context.
+  //
+
+  /**
+   * Fetches the user permissions.
+   */
+  getContextData(): void {
+    this.zaakService.listCaseUsers(this.zaak.bronorganisatie, this.zaak.identificatie).subscribe(
+      (userPermissions: UserPermission[]): void => {
+        this.table = this.userPermissionsAsTable(userPermissions);
+
+        this.shortTable = {...this.table};
+        this.shortTable.bodyData = this.shortTable.bodyData.slice(0, 3);
+
+        this.isLoading = false;
+      },
+      this.reportError.bind(this)
+    );
+  }
+
+
+  /**
+   * Returns user permissions as table.
+   * @param {UserPermission[]} userPermissions
+   * @return {Table}
+   */
+  userPermissionsAsTable(userPermissions: UserPermission[]): Table {
+    const bodyData = userPermissions.reduce((acc, userPermission) => {
+      const userRows = userPermission.permissions.map((permission: ZaakPermission) => ({
+        cellData: {
+          user: {
+            type: 'text',
+            label: userPermission.username,
+          },
+          permission: {
+            type: 'chip',
+            label: permission.permission
+          },
+          // Hide button if case is closed and the user is not allowed to force edit
+          delete: !this.zaak.resultaat || this.zaak.kanGeforceerdBijwerken ? {
+            type: 'button',
+            label: 'Verwijderen',
+            value: permission,
+          } : ''
+        },
+        nestedTableData: new Table(['Reden', 'Commentaar'], [{
+          cellData: {
+            reason: permission.reason,
+            comment: permission["comment"]
+          }
+        },
+        ]),
+        expandData: ''
+      }));
+      return [...acc, ...userRows];
+    }, []);
+
+    return new Table(
+      [
+        'Gebruiker',
+        'Rechten',
+        'Acties',
+      ], bodyData);
+  }
+
+  //
+  // Events.
+  //
+
+  /**
+   * Opens modal.
+   * @param {string} id
+   */
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  /**
+   * Expand table rows.
+   */
+  expand() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  /**
+   * Get called when the remove permission button (in the table) is clicked.
+   * Opens the confirm dialog.
+   * @param event
+   */
+  buttonClick(event) {
+    this.selectedPermission = event.delete;
+    this.modalService.open('delete-permission-modal')
+  }
+
+  /**
+   * Deletes user permission.
+   */
+  deletePermission(): void {
+    const permission = this.selectedPermission;
+    this.permissionsService.deletePermission(permission).subscribe(
+      (): void => {
+        this.getContextData();
+        this.closeDeletePermission();
+      },
+      this.reportError.bind(this),
+    );
+  }
+
+  /**
+   * Closes the delete permission dialog.
+   */
+  closeDeletePermission() {
+    this.selectedPermission = null;
+    this.modalService.close('delete-permission-modal');
+  }
 
   /**
    * Error callback.
@@ -187,7 +204,7 @@ export class UserPermissionsComponent implements OnInit {
   reportError(error: any): void {
     this.isLoading = false;
 
-    if(error.error?.detail) {
+    if (error.error?.detail) {
       this.errorDetailMessage = error.error?.detail;
     } else {
       console.error(error);
