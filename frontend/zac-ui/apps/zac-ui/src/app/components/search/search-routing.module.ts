020f28653650a62b7abc810ffff92f495331fa28
Kelvin Chan <kelvinchancy95@gmail.com>
2022-11-23 22:37:42 +0100

    1840 - allow deeplink in search

--- frontend/zac-ui/apps/zac-ui/src/app/components/search/search-routing.module.ts
+++ frontend/zac-ui/apps/zac-ui/src/app/components/search/search-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: SearchComponent,
-  },
+  { path: '', redirectTo: 'zaak', pathMatch: 'full' },
+  { path: '', children: [
+      { path: 'zaak', component: SearchComponent },
+      { path: 'object', component: SearchComponent }
+    ]
+  }
 ];
 
 /**
--- frontend/zac-ui/libs/features/search/src/lib/search-form/search-form.component.ts
+++ frontend/zac-ui/libs/features/search/src/lib/search-form/search-form.component.ts
@@ -4,6 +4,8 @@ import {SnackbarService} from '@gu/components';
 import {Zaak, TableSort, ZaakObject} from '@gu/models';
 import {SearchService} from '../search.service';
 import {MapGeometry, MapMarker} from "../../../../../shared/ui/components/src/lib/components/map/map";
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 
 /**
@@ -22,18 +24,40 @@ import {MapGeometry, MapMarker} from "../../../../../shared/ui/components/src/li
 export class SearchFormComponent {
   readonly errorMessage = 'Er is een fout opgetreden bij het zoeken naar zaken.'
 
-  isLoading: boolean;
-
   @Input() sortData: TableSort;
-  @Input() pageData: PageEvent;
 
+  @Input() pageData: PageEvent;
   @Output() loadResult: EventEmitter<Zaak[]> = new EventEmitter<Zaak[]>();
   @Output() resultLength: EventEmitter<number> = new EventEmitter<number>();
 
   @Output() mapGeometry: EventEmitter<MapGeometry> = new EventEmitter<MapGeometry>();
   @Output() mapMarkers: EventEmitter<MapMarker[]> = new EventEmitter<MapMarker[]>();
 
-  constructor(private searchService: SearchService, private snackbarService: SnackbarService) {
+  isLoading: boolean;
+
+  /** Tabs */
+  tabs: object[] = [
+    {
+      link: 'zaak',
+      title: 'Zoeken op zaak'
+    },
+    {
+      link: 'object',
+      title: 'Zoeken op object'
+    }
+  ]
+
+  /** Active tab */
+  activatedChildRoute: string;
+
+  constructor(
+    private location: Location,
+    private route: ActivatedRoute,
+    private searchService: SearchService,
+    private snackbarService: SnackbarService) {
+    route.url.subscribe(() => {
+      this.activatedChildRoute = route.snapshot.url[0].path;
+    });
   }
 
   //
@@ -67,6 +91,23 @@ export class SearchFormComponent {
       },
       this.reportError.bind(this)
     );
+  }
+
+  /**
+   * Create tab link
+   * @param tab
+   * @returns {string}
+   */
+  getTabLink(tab) {
+    return `/zoeken/${tab}`;
+  }
+
+  /**
+   * Redirect to url
+   * @param tab
+   */
+  setUrl(tab) {
+    this.location.go(this.getTabLink(tab))
   }
 
   /**
