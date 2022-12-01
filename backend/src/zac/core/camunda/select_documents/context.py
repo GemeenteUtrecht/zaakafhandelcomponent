a46a1a599e096847f7ece129d6a366d66435ff7b
damm89 <danielammeraal@gmail.com>
2022-12-01 11:25:50 +0100

    :sparkles: Allow users to select more documents from hoofdzaak in bijdragezaak

--- backend/src/zac/core/camunda/select_documents/context.py
+++ backend/src/zac/core/camunda/select_documents/context.py
@@ -25,6 +25,16 @@ def get_context(task: Task) -> DocumentSelectContext:
     zaak_url = get_process_zaak_url(task, zaak_url_variable="hoofdZaakUrl")
     zaak = get_zaak(zaak_url=zaak_url)
     documenten, gone = get_documenten(zaak)
+
+    bijdrage_zaak_url = get_process_zaak_url(task, zaak_url_variable="zaakUrl")
+    bijdrage_zaak = get_zaak(zaak_url=bijdrage_zaak_url)
+    bijdrage_zaak_document_urls = [
+        doc.titel for doc in get_documenten(bijdrage_zaak)[0]
+    ]
+    documenten = [
+        doc for doc in documenten if doc.titel not in bijdrage_zaak_document_urls
+    ]
+
     resolved_documenten = resolve_documenten_informatieobjecttypen(documenten)
     related_zaaktype = get_zaaktype_from_identificatie(task)
     informatieobjecttypen = get_informatieobjecttypen_for_zaaktype(related_zaaktype)
