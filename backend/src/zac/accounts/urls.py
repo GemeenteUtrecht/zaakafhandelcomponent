c17a88f19dc498a6f6bba3db50a6f71cc8c5a481
Sven van de Scheur <svenvandescheur@gmail.com>
2022-12-01 16:20:08 +0100

    :sparkles: - Redirect after adfs login.

--- backend/src/zac/accounts/urls.py
+++ backend/src/zac/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import LoginView
+from .views import LoggedInView, LoginView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
+    path("logged_in/", LoggedInView.as_view(), name="logged_in"),
 ]
--- backend/src/zac/accounts/views.py
+++ backend/src/zac/accounts/views.py
@@ -1,5 +1,22 @@
 from django.contrib.auth.views import LoginView as _LoginView
+from django.views.generic import RedirectView
 
 
 class LoginView(_LoginView):
     template_name = "accounts/login.html"
+
+    def get(self, request, *args, **kwargs):
+        login_next = self.request.GET.get("next")
+        request.session["login_next"] = login_next
+        return super().get(request, *args, **kwargs)
+
+
+class LoggedInView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        try:
+            login_next = self.request.session.pop("login_next")
+            print(login_next)
+
+            return login_next
+        except KeyError:
+            return "/ui/"
