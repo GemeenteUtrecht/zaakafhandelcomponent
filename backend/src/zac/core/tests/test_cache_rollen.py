b8a366fd67ffda86fc1b06aab3bb18e63d3c2b16
damm89 <danielammeraal@gmail.com>
2022-12-15 12:18:25 +0100

    :white_check_mark: Remove tests for invalidating get_rollen function

--- backend/src/zac/core/tests/test_cache_rollen.py
+++ backend/src/zac/core/tests/test_cache_rollen.py
@@ -20,46 +20,6 @@ ZAAK_URL = f"{ZAKEN_ROOT}zaken/482de5b2-4779-4b29-b84f-add888352182"
 
 @requests_mock.Mocker()
 class TestCacheRollen(ClearCachesMixin, APITransactionTestCase):
-    def test_get_rollen_cache(self, m):
-        Service.objects.create(api_type=APITypes.zrc, api_root=ZAKEN_ROOT)
-        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
-        rollen = [
-            generate_oas_component(
-                "zrc",
-                "schemas/Rol",
-                zaak=ZAAK_URL,
-                betrokkene_identificatie={
-                    "identificatie": "123456",
-                    "voorletters": "M.Y.",
-                    "achternaam": "Surname",
-                    "voorvoegsel_achternaam": "",
-                },
-            )
-        ]
-        paginated_response = {
-            "count": 0,
-            "next": None,
-            "previous": None,
-            "results": rollen,
-        }
-        m.get(f"{ZAKEN_ROOT}rollen?zaak={ZAAK_URL}", json=paginated_response)
-
-        # Make sure rollen arent cached
-        _cache = caches["request"]
-        self.assertFalse(f"rollen:{ZAAK_URL}" in _cache)
-        zaak = generate_oas_component(
-            "zrc",
-            "schemas/Zaak",
-            url=ZAAK_URL,
-        )
-
-        # Make call
-        rollen = get_rollen(factory(Zaak, zaak))
-
-        # Assert rollen are now in cache
-        self.assertTrue(f"rollen:{ZAAK_URL}" in _cache)
-        self.assertEqual(rollen, _cache.get(f"rollen:{ZAAK_URL}"))
-
     def test_fetch_rol_cache(self, m):
         Service.objects.create(api_type=APITypes.zrc, api_root=ZAKEN_ROOT)
         mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
@@ -86,53 +46,6 @@ class TestCacheRollen(ClearCachesMixin, APITransactionTestCase):
         # Assert rol is now cached
         self.assertTrue(f"rol:{rol['url']}" in cache)
         self.assertEqual(fetched_rol, cache.get(f"rol:{rol['url']}"))
-
-    def test_invalidate_get_rollen_cache(self, m):
-        Service.objects.create(api_type=APITypes.zrc, api_root=ZAKEN_ROOT)
-        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
-        rollen = [
-            generate_oas_component(
-                "zrc",
-                "schemas/Rol",
-                zaak=ZAAK_URL,
-                betrokkene_identificatie={
-                    "identificatie": "123456",
-                    "voorletters": "M.Y.",
-                    "achternaam": "Surname",
-                    "voorvoegsel_achternaam": "",
-                },
-                omschrijving="ene-rol",
-            )
-        ]
-        paginated_response = {
-            "count": 0,
-            "next": None,
-            "previous": None,
-            "results": rollen,
-        }
-        m.get(f"{ZAKEN_ROOT}rollen?zaak={ZAAK_URL}", json=paginated_response)
-
-        # Make sure rollen arent cached
-        _cache = caches["request"]
-        self.assertFalse(f"rollen:{ZAAK_URL}" in _cache)
-        zaak = generate_oas_component(
-            "zrc",
-            "schemas/Zaak",
-            url=ZAAK_URL,
-        )
-        zaak = factory(Zaak, zaak)
-
-        # Make call
-        old_rollen = get_rollen(zaak)
-
-        # Assert rollen are now in cache
-        self.assertTrue(f"rollen:{ZAAK_URL}" in _cache)
-        self.assertEqual(old_rollen, _cache.get(f"rollen:{ZAAK_URL}"))
-
-        invalidate_rollen_cache(zaak)
-
-        # Assert rollen are now removed from cache
-        self.assertFalse(f"rollen:{ZAAK_URL}" in _cache)
 
     def test_invalidate_fetch_rol_cache(self, m):
         Service.objects.create(api_type=APITypes.zrc, api_root=ZAKEN_ROOT)
