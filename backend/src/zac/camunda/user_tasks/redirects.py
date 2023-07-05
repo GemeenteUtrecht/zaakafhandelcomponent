97b017542b148c25440e932d602d4059b7b24659
damm89 <danielammeraal@gmail.com>
2023-07-05 13:24:36 +0200

    :sparkles: Allow the possibility of user tasks to not be finished by a user

--- backend/src/zac/camunda/user_tasks/redirects.py
+++ backend/src/zac/camunda/user_tasks/redirects.py
@@ -14,13 +14,14 @@ from . import Context, register, usertask_context_serializer
 class RedirectContext(Context):
     redirect_to: str
     open_in_new_window: bool
+    end_task: bool
 
 
 @usertask_context_serializer
 class RedirectContextSerializer(APIModelSerializer):
     class Meta:
         model = RedirectContext
-        fields = ("redirect_to", "open_in_new_window")
+        fields = ("redirect_to", "open_in_new_window", "end_task")
 
 
 @register("zac:doRedirect", RedirectContextSerializer)
@@ -30,8 +31,7 @@ def get_redirect_task_context(task: Task) -> RedirectContext:
     if query_params:
         redirect_url = furl(redirect_url).add(query_params).url
     new_window = task.get_variable("openInNewWindow", default=False)
-
+    end_task = task.get_variable("endTask", default=True)
     return RedirectContext(
-        redirect_to=redirect_url,
-        open_in_new_window=new_window,
+        redirect_to=redirect_url, open_in_new_window=new_window, end_task=end_task
     )
