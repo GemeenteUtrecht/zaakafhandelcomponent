1527eff1922cf5b41f190defd40afbf37fcf58c3
damm89 <52245527+damm89@users.noreply.github.com>
2023-07-27 15:12:56 +0200

    Config/hijack (#1041)
    * :sparkles: redirect to admin after hijack release
    
    * :fire: Remove unused code and imports
    
    * :rewind: Changes to package-lock.json

--- backend/src/zac/accounts/middleware.py
+++ backend/src/zac/accounts/middleware.py
@@ -1,42 +1,11 @@
 from django.conf import settings
-from django.urls import reverse_lazy
-from django.utils.crypto import salted_hmac
 
 from hijack.middleware import HijackUserMiddleware as _HijackUserMiddleware
 from mozilla_django_oidc_db.middleware import SessionRefresh
 
-from zac.accounts.models import User
 
-
-class FirstRedirectCheckMixin:
-    key_salt = settings.SECRET_KEY
-
-    def _make_hash_value(self, user: User) -> str:
-        """
-        Hash the:
-            user primary key and password,
-
-        """
-        return str(user.pk) + user.password
-
-    def make_first_redirect_key(self, user: User) -> str:
-        return salted_hmac(
-            self.key_salt,
-            self._make_hash_value(user),
-        ).hexdigest()[::2]
-
-
-class HijackUserMiddleware(FirstRedirectCheckMixin, _HijackUserMiddleware):
+class HijackUserMiddleware(_HijackUserMiddleware):
     header = settings.HIJACK_HEADER
-    release_url = reverse_lazy("hijack:release")
-
-    def process_request(self, request):
-        super().process_request(request)
-        if request.path == self.release_url:
-            if request.method == "POST":
-                request.session[
-                    "first_redirect_after_hijack"
-                ] = self.make_first_redirect_key(request.user)
 
     def process_response(self, request, response):
         response = super().process_response(request, response)
@@ -45,19 +14,9 @@ class HijackUserMiddleware(FirstRedirectCheckMixin, _HijackUserMiddleware):
         return response
 
 
-class HijackSessionRefresh(FirstRedirectCheckMixin, SessionRefresh):
+class HijackSessionRefresh(SessionRefresh):
     def process_request(self, request):
-        if request.user.is_authenticated:
-            first_redirect = request.session.get("first_redirect_after_hijack", False)
-            hijack_history = bool(request.session.get("hijack_history", []))
-            if first_redirect and hijack_history:
-                return
-
-            if first_redirect == self.make_first_redirect_key(request.user):
-                del request.session["first_redirect_after_hijack"]
-                return
-
-            if hijack_history:
-                return
+        if bool(request.session.get("hijack_history", [])):
+            return
 
         return super().process_request(request)
