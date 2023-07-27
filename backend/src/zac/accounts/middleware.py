bae4a6e2f05c4228750d5258112f919ddfb85ddd
damm89 <danielammeraal@gmail.com>
2023-07-27 15:09:07 +0200

    :fire: Remove unused code and imports

--- backend/src/zac/accounts/middleware.py
+++ backend/src/zac/accounts/middleware.py
@@ -1,31 +1,10 @@
 from django.conf import settings
-from django.utils.crypto import salted_hmac
 
 from hijack.middleware import HijackUserMiddleware as _HijackUserMiddleware
 from mozilla_django_oidc_db.middleware import SessionRefresh
 
-from zac.accounts.models import User
 
-
-class FirstRedirectCheckMixin:
-    key_salt = settings.SECRET_KEY
-
-    def _make_hash_value(self, user: User) -> str:
-        """
-        Hash the:
-            user primary key and password,
-
-        """
-        return str(user.pk) + user.password
-
-    def make_first_redirect_key(self, user: User) -> str:
-        return salted_hmac(
-            self.key_salt,
-            self._make_hash_value(user),
-        ).hexdigest()[::2]
-
-
-class HijackUserMiddleware(FirstRedirectCheckMixin, _HijackUserMiddleware):
+class HijackUserMiddleware(_HijackUserMiddleware):
     header = settings.HIJACK_HEADER
 
     def process_response(self, request, response):
@@ -35,7 +14,7 @@ class HijackUserMiddleware(FirstRedirectCheckMixin, _HijackUserMiddleware):
         return response
 
 
-class HijackSessionRefresh(FirstRedirectCheckMixin, SessionRefresh):
+class HijackSessionRefresh(SessionRefresh):
     def process_request(self, request):
         if bool(request.session.get("hijack_history", [])):
             return
