82d8916132abbb27310ad85d9cc2828d5a34dbf1
damm89 <danielammeraal@gmail.com>
2023-09-18 16:06:02 +0200

    :white_check_mark: Pass tests

--- backend/src/zac/accounts/tests/test_command_add_atomic_permissions.py
+++ backend/src/zac/accounts/tests/test_command_add_atomic_permissions.py
@@ -35,7 +35,6 @@ class AddPermissionCommandTests(ClearCachesMixin, TestCase):
         config.save()
 
         cls.user = UserFactory.create(username="test_user")
-        cls.token = Token.objects.get_or_create(user=cls.user)
 
     def test_add_permission_for_behandelaar(self, m):
         # mock API requests
