091dbc2f17e41a4c6d3eb7896a44240b807ce7e7
damm89 <danielammeraal@gmail.com>
2023-10-02 15:06:56 +0200

    :pencil2: fix folder typo

--- backend/README.rst
+++ backend/README.rst
@@ -2,7 +2,7 @@
 Zaakafhandelcomponent
 =====================
 
-:Version: 0.75.9
+:Version: 0.75.10
 :Source: https://github.com/GemeenteUtrecht/zaakafhandelcomponent
 :Keywords: zaken, zaakgericht werken, GEMMA, Utrecht
 :PythonVersion: 3.9
--- backend/doc/conf.py
+++ backend/doc/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Maykin Media, Gemeente Utrecht"
 author = "Maykin Media, Gemeente Utrecht"
 
 # The full version, including alpha/beta/rc tags
-release = "0.75.9"
+release = "0.75.10"
 
 
 # -- General configuration ---------------------------------------------------
