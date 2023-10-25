3b9483cb3f7d79d57ef4290094f417a54aa58f05
Kelvin Chan <kelvinchancy95@gmail.com>
2023-10-25 09:02:41 +0200

    CORS fix attempt

--- frontend/zac-ui/apps/zac-ui/src/app/helpers/auth.interceptor.ts
+++ frontend/zac-ui/apps/zac-ui/src/app/helpers/auth.interceptor.ts
@@ -18,19 +18,17 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private router: Router) { }
 
   private checkAuthentication(error: HttpErrorResponse): boolean {
-    console.log((error.status && error.status === 403 && error.error.detail === this.NOT_LOGGED_IN_MESSAGE) || (error.status && error.status === 0));
-    return (error.status && error.status === 403 && error.error.detail === this.NOT_LOGGED_IN_MESSAGE) || (error.status && error.status === 0);
+    return error.status && error.status === 403 && error.error.detail === this.NOT_LOGGED_IN_MESSAGE;
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req)
       .pipe(
         catchError((error, caught) => {
-          console.log(error);
-          console.log(caught);
           if (error instanceof HttpErrorResponse) {
-            if (this.checkAuthentication(error)) {
-              console.log(2);
+            console.log(error);
+            console.log(error.status);
+            if (this.checkAuthentication(error) || error.status === 0) {
               const currentPath = this.router.url;
               window.location.href = `/accounts/login/?next=/ui${currentPath}`;
               return throwError(error);
