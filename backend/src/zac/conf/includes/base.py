8db695331e12320bc8f24ff47cecfb210d64c0e5
damm89 <danielammeraal@gmail.com>
2023-11-08 10:32:02 +0100

    :mute: Remove annoying debug logs for mozilla_django_oidc..

--- backend/src/zac/conf/includes/base.py
+++ backend/src/zac/conf/includes/base.py
@@ -340,7 +340,7 @@ LOGGING = {
         },
         "mozilla_django_oidc": {
             "handlers": ["console"],
-            "level": "DEBUG",
+            "level": "WARNING",
             "propagate": True,
         },
     },
