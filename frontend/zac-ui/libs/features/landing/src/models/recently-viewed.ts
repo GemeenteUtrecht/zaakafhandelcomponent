fc97931838f4a4f7bd561d7fdc67f7a75fca5769
damm89 <danielammeraal@gmail.com>
2024-12-06 15:52:41 +0100

    :ok_hand: Process feedback regarding recently viewed section on landing page

--- frontend/zac-ui/libs/features/landing/src/models/recently-viewed.ts
+++ frontend/zac-ui/libs/features/landing/src/models/recently-viewed.ts
@@ -1,7 +1,9 @@
 export interface RecentlyViewed {
   "visited": Date,
   "identificatie": string,
-  "url": string
+  "url": string,
+  "omschrijving": string,
+  "zaaktypeOmschrijving": string
 }
 
 export interface RecentlyViewedCases {
