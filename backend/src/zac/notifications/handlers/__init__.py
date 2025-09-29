cafe159297cf67c052368ba556c91de0c079a8f8
damm89 <danielammeraal@gmail.com>
2025-09-29 18:09:04 +0200

    fix import

--- backend/src/zac/notifications/handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-# convenience re-exports
-from .documenten import InformatieObjectenHandler
-from .informatieobjecttypen import InformatieObjecttypenHandler
-from .objecten import ObjectenHandler
-from .zaaktypen import ZaaktypenHandler
-from .zaken import ZakenHandler
